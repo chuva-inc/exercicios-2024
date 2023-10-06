e14600bdcf7d0cc0a649d7ef3b3eca55310c1df6
Ryan Iug <ryaniugramos@gmail.com>
2023-10-06 19:10:10 -0300

    sidebar created

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { WorkComponent } from './components/pages/work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule
