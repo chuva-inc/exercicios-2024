20d1cac353929f686dedc0f7e4baba74bc04df45
Marcos Túlio <dev@marcostulio.in>
2023-10-08 14:04:28 -0300

    feat: finalizada implementação da lógica de Web Scraper na classe Scrapper

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -10,21 +10,54 @@ use Chuva\Php\WebScrapping\Entity\Person;
  */
 class Scrapper {
 
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
-  public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
-  }
+    /**
+     * Loads paper information from the HTML and returns the array with the data.
+     */
+    public function scrap(\DOMDocument $dom): array {
+
+        /**
+         * Query and extract text content with XPath
+         *
+         * @var / $xpath
+         */
+        $xpath = new \DOMXPath($dom);
+        $all_ids = $xpath->query('//div[@class="volume-info"]');
+        $all_titles = $xpath->query('//h4[@class="my-xs paper-title"]');
+        $all_types = $xpath->query('//div[@class="tags mr-sm"]');
+        $all_div_authors = $xpath->query('//div[@class="authors"]');
+
+        /**
+         * Iterating elements and return data
+         *
+         * @var  $data
+         */
+        $data = [];
+
+        for ($i = 0; $i < $all_ids->length; $i++) {
+
+            $id = $all_ids->item($i)->textContent;
+            $title = $all_titles->item($i)->textContent;
+            $type = $all_types->item($i)->textContent;
+            $authorSpans = $xpath->query('.//span', $all_div_authors->item($i));
+
+            /**
+             * Iterating spans and assign authors
+             *
+             * @var  $authors
+             */
+            $authors = [];
+            foreach ($authorSpans as $authorSpan) {
+
+                $authorName = $authorSpan->textContent;
+                $authorInstitution = $authorSpan->getAttribute("title");
+                $authors[] = new Person($authorName, $authorInstitution);
+            }
+
+            $paper = new Paper($id, $title, $type, $authors);
+            $data[] = $paper;
+        }
+
+        return $data;
+    }
 
 }
