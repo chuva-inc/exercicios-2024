dd32bcbc42d510a6b409ac741b0ec36d11e1bf1d
Gustavo Oliveira Viana <68523116+VianaGu@users.noreply.github.com>
2023-10-08 08:08:01 -0300

    Testando Main.php no action

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -12,12 +12,12 @@ class Main {
    */
   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+    $dom->loadHTMLFile(__DIR__ . 'assets/origin.html');
 
     $data = (new Scrapper())->scrap($dom);
 
     // Write your logic to save the output file bellow.
-    print_r($data);
+    print_r($data);   
   }
 
 }
