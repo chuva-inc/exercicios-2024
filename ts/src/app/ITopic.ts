951a3e14c324f08bd4aacf78576214858259af90
Ryan Iug <ryaniugramos@gmail.com>
2023-10-08 18:13:47 -0300

    border of discussions and summary section, card created, card container created

---
+++ ts/src/app/ITopic.ts
@@ -0,0 +1,7 @@
+export interface ITopic {
+  subject: string;
+  author: string;
+  phrase: string;
+  likes: number;
+  answers: number;
+}
--- ts/src/app/components/card/card.component.ts
+++ ts/src/app/components/card/card.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ITopic } from 'src/app/ITopic';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
+  topic: ITopic = {
+    subject: 'Assunto da pergunta aparece aqui',
+    author: 'Carlos Henrique Santos',
+    phrase:
+      'Comecinho da pergunta aparece aqui resente relato inscreve-se no campo da análise da dimensão e impacto de processo formativo situado impacto de processo formativo processo...',
+    likes: 1,
+    answers: 1,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
