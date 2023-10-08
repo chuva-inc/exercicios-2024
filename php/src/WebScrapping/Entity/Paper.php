e4ccc514fa7248b3d29c2c9d4615dd5495f8b826
Marcos Túlio <dev@marcostulio.in>
2023-10-08 13:06:34 -0300

    feat: contructor da classe Paper corrigido e metodos adicionados

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -7,38 +7,63 @@ namespace Chuva\Php\WebScrapping\Entity;
  */
 class Paper {
 
-  /**
-   * Paper Id.
-   *
-   * @var int
-   */
-  public $id;
+    /**
+     * Paper Id.
+     *
+     * @var int
+     */
+    public int $id;
 
-  /**
-   * Paper Title.
-   *
-   * @var string
-   */
-  public $title;
+    /**
+     * Paper Title.
+     *
+     * @var string
+     */
+    public string $title;
 
-  /**
-   * The paper type (e.g. Poster, Nobel Prize, etc).
-   *
-   * @var string
-   */
-  public $type;
+    /**
+     * The paper type (e.g. Poster, Nobel Prize, etc).
+     *
+     * @var string
+     */
+    public string $type;
 
-  /**
-   * Paper authors.
-   *
-   * @var \Chuva\Php\WebScrapping\Entity\Person[]
-   */
-  public $authors;
+    /**
+     * Paper authors.
+     *
+     * @var Person[]
+     */
+    public mixed $authors;
 
-  /**
-   * Builder.
-   */
-  public function __construct($id, $title, $type, $authors = []) {
-  }
+    /**
+     * Builder.
+     */
+    public function __construct($id, $title, $type, $authors = []) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->type = $type;
+        $this->authors = $authors;
+
+    }
+
+    /**
+     * Methods.
+     */
+    public function getId() {
+        return $this->id;
+
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function getAuthors() {
+        return $this->authors;
+    }
 
 }
