add624bed72c9539a9052d6be008a4a778b91619
Marcos Túlio <dev@marcostulio.in>
2023-10-09 00:05:17 -0300

    feat: finalizada implementação da lógica para escrever os dados na planilha com Spout

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,22 +2,114 @@
 
 namespace Chuva\Php\WebScrapping;
 
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+
 /**
  * Runner for the Webscrapping exercice.
  */
 class Main {
 
-  /**
-   * Main runner, instantiates a Scrapper and runs.
-   */
-  public static function run(): void {
-    $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+    /**
+     * Main runner, instantiates a Scrapper and runs.
+     */
+    public static function run(): void {
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
+        /**
+         * data call the method scrap of Scrapper class
+         *
+         * @var  $data
+         */
+        $data = (new Scrapper())->scrap($dom);
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
-  }
+        $outputFilePath = "GaloScrapper_MarcosTulio.xlsx";
+
+        /**
+         * Writer XLSX using Spout.
+         *
+         * @var  $writer
+         */
+        $writer = WriterEntityFactory::createXLSXWriter();
+        $writer->openToFile($outputFilePath);
+
+        $header = [
+            "ID",
+            "Title",
+            "Type",
+        ];
+
+        $maxAuthors = 0;
+
+        /**
+         * Iterating in all objects and counting just authors number
+         *
+         * @var  $paper
+         */
+        foreach ($data as $paper) {
+            $numAuthors = count($paper->getAuthors());
+            if ($numAuthors > $maxAuthors) {
+                $maxAuthors = $numAuthors;
+            }
+        }
+
+        for ($i = 1; $i <= $maxAuthors; $i++) {
+
+            /**
+             * Loop to create headers for authors and institutions.
+             * Each iteration generates a pair of headers.
+             */
+            $header[] = "Author $i";
+            $header[] = "Author $i Institution";
+        }
+
+        /**
+         * Creates a header row for the file.
+         *
+         * @var  $headerRow
+         */
+        $headerRow = WriterEntityFactory::createRowFromArray($header);
+        $writer->addRow($headerRow);
+
+        foreach ($data as $paper) {
+            $rowData = [
+                $paper->getId(),
+                $paper->getTitle(),
+                $paper->getType(),
+            ];
+
+            /**
+             * Iterating all objects and return authors and institutions
+             *
+             * @var  $author
+             */
+            foreach ($paper->getAuthors() as $author) {
+                /**
+                 * The method returns the name of the author
+                 *
+                 * @var  $authorName
+                 */
+                $authorName = $author->getName();
+                $institution = $author->getInstitution();
+
+                /**
+                 * Making assignments to the array
+                 */
+                $rowData[] = $authorName;
+                $rowData[] = $institution;
+            }
+
+            /**
+             * Creating the rows from array $rowData
+             *
+             * @var  $dataRow
+             */
+            $dataRow = WriterEntityFactory::createRowFromArray($rowData);
+            $writer->addRow($dataRow);
+        }
+
+        $writer->close();
+        echo "Dados salvos em $outputFilePath\n";
+    }
 
 }
