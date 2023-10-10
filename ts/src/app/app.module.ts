8e060ae258c6d027b996779b547d2f90e1da440d
sirguy12 <ryankairo09@yahoo.com.br>
2023-10-10 04:10:08 -0300

    side finished

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
