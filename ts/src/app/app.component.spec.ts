4e99019574f94c75f4c433c56c5e4a7236516143
Jimmy ABF <122823625+jimmycalhado@users.noreply.github.com>
2023-10-12 21:49:31 -0300

    Atualizando os arquivos html, scss e ts

--- ts/src/app/app.component.spec.ts
+++ ts/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ts app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ts app is running!');71
   });
 });
--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevChuva';
+
+mostrar(el: string): void{
+  const element = document.getElementById(el);
+  if  (element){
+    element.style.display = 'block';
+  }
 }
+aumentar(el: string): void{
+  const element = document.getElementById(el);
+  if(element){
+    element.style.height = '466px';
+  }
+}
+sumir(el: string): void{
+  const element = document.getElementById(el);
+  if (element) {
+    element.style.display = 'none';
+  }
+  }
+heightcom(el: string): void{
+  const element = document.getElementById(el);
+  if(element){
+    element.style.height = '969px';
+  }
+}
+heightdisc(el: string): void{
+  const element = document.getElementById(el);
+  if(element){
+    element.style.height = '1050px';
+  }
+}
+
+}
+
+
+
