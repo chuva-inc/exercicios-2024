519bc1ce57b89e8b6aaabab7e113ef04eb33365d
SuzuhRevv <joaopk04@hotmail.com>
2023-10-17 03:20:53 -0300

    Praticamente finalizado o  HTML e CSS para a criação de tópicos além da adição de uma função que manipula a DOM para fazer esse fluxo de criação de tópico

--- ts/src/app/app.component.js
+++ ts/src/app/app.component.js
@@ -1,6 +1,8 @@
 const btnShowMore = document.querySelectorAll('.btn-show-more');
 const hiddenText = document.querySelectorAll('.aditional-content');
 
+const btnCreateTopic = document.querySelectorAll('.btn-create-topic')
+
 btnShowMore.forEach(function (btn, index) {
   btn.addEventListener('click', function () {
     if (hiddenText[index].style.display === 'none' || hiddenText[index].style.display === '') {
@@ -12,3 +14,9 @@ btnShowMore.forEach(function (btn, index) {
     }
   });
 });
+
+btnCreateTopic.forEach(function (btn, index) {
+    btn.addEventListener('click', function () {
+        
+    })
+})
