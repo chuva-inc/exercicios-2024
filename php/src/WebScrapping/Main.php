881279d11fb32ff851caf1b572c1a532b2445992
Gustavo Oliveira Viana <68523116+VianaGu@users.noreply.github.com>
2023-10-17 08:53:28 -0300

    Tentando escrever na tabela

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,13 +2,7 @@
 
 namespace Chuva\Php\WebScrapping;
 
-use Box\Spout\Common\Entity\Row;
-use Box\Spout\Common\Exception\IOException;
-use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
-use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
-use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
-use Chuva\Php\WebScrapping\Entity\Paper;
-use Chuva\Php\WebScrapping\Entity\Person;
+
 /**
  * Runner for the Webscrapping exercice.
  */
@@ -23,70 +17,9 @@ class Main {
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
     
      # Create empty lists to store the data
-     $id = [];
-     $title = [];
-     $type = [];
-     $autores = "";
-     $instituicao = "";
     // Write your logic to save the output file bellow.   
     # Create an XPath object
-    $xpath = new \DOMXPath($dom);
-
-    # Query the HTML for the paper cards
-    $paper_card = $xpath->query('.//a[@class="paper-card p-lg bd-gradient-left"]');
-    # Query the HTML for the paper IDs
-    $ID = $xpath->query('.//div[@class="volume-info"]');
-    # Query the HTML for the paper types
-    $TYPE =$xpath->query('.//div[@class="tags mr-sm"]');
-           
-
-                
-    
-    # Iterate over the paper types
-    foreach ($TYPE as $currentType) {
-      $type[] = $currentType->textContent;
-      print_r("Tipo: " . $type[1] . "\n");
-    }
-    # Iterate over the paper IDs
-    foreach ($ID as $currentId) {
-      $id[] = $currentId->textContent;
-      print_r("ID:" . $id[1] . "\n");
-    } 
-    # Iterate over the paper cards
-    foreach ($paper_card as $elemento) {
-      # Iterate over the child nodes of the paper card 
-      foreach ($elemento->childNodes as $node) {
-        # Get the tag name of the child node
-        $node_tagname = $node->tagName; 
-          # If the tag name is 'h4', then it is the title of the paper
-          if($node_tagname == 'h4'){
-            # Print the title of the paper
-            $title = $node->nodeValue;
-            print_r("Titulo: $title \n");
-          } 
-          # If the tag name is 'div', then it is the title of the paper
-          else if($node_tagname == 'div'){          
-            $filhoNo = $node->childNodes;
-            foreach ($filhoNo as $no){
-              print_r($no);
-              $no_tagname = $no->tagName;
-              # If the tag name is 'div', then it is the title of the paper
-              if($no_tagname=='span'){
-                if(($no->tagName)!=NULL) $instituicao = $no->getAttribute('title');
-                print_r($instituicao . "\n");    
-                $autores = $no->textContent;
-                print_r("Autores: $autores \n");
-                $person = new Person($autores, $instituicao);
-              }else if($no_tagname==NULL){
-                continue;
-              }
-            }
-            
-          }
-          
-      } 
-      /* Função teste $paper = new Paper($id,$title,$type,)  */
-    } 
+    $data = (new Scrapper())->scrap($dom);
     
   }  
 }
@@ -98,25 +31,7 @@ class Main {
   
     
 
-      $filePath = 'exemplo.xlsx';
-      $writer = WriterEntityFactory::createXLSXWriter();
-      $writer->openToFile($filePath);
-
-      $style = (new StyleBuilder())
-          ->setFontBold()
-          ->build();
-
-      $headerRow = WriterEntityFactory::createRowFromArray(['ID','Title',	'Type',	'Author 1',	'Author 1 Institution',	'Author 2'	,'Author 2 Institution', 'Author 3','Author 3','Author 3 Institution'], $style);
-      $writer->addRow($headerRow); 
-
-
-      foreach ($elements as $element) {
-          $writer->addRow($data);
-      }
-
-      $writer->close();
-
-      echo "Tabela criada com sucesso em $filePath \n" ;
+      
     /* print_r($data);  */ 
  
 ?>
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -4,6 +4,14 @@ namespace Chuva\Php\WebScrapping;
 
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
+use Box\Spout\Common\Entity\Row;
+use Box\Spout\Common\Exception\IOException;
+use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
+use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Reader\ReaderFactory;
+use Box\Spout\Common\Type;
+
 
 /**
  * Does the scrapping of a webpage.
@@ -14,17 +22,133 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+    $xpath = new \DOMXPath($dom);
+
+    # Query the HTML for the paper cards
+    $paper_card = $xpath->query('.//a[@class="paper-card p-lg bd-gradient-left"]');
+           
+
+                
+   
+    try{
+      # Iterate over the paper cards
+      foreach ($paper_card as $elemento) {
+        # Iterate over the child nodes of the paper card 
+        foreach ($elemento->childNodes as $node) {
+          # Get the tag name of the child node
+          $node_tagname = $node->tagName; 
+            # If the tag name is 'h4', then it is the title of the paper
+            if($node_tagname == 'h4'){
+              # Print the title of the paper
+              $title = $node->nodeValue;
+              print_r("Titulo: $title \n");
+            } 
+            # If the tag name is 'div', then it is the title of the paper
+            else if($node_tagname == 'div'){          
+              $filhoNo = $node->childNodes;
+              foreach ($filhoNo as $no){
+                # If the tag name is 'div', then it is the title of the paper     
+                if($no->tagName=='span'){
+                  $instituicao = $no->getAttribute('title');
+                  print_r("Instituição: $instituicao \n");    
+                  $autor = $no->textContent;
+                  print_r("Autores: $autor \n");
+                  $person[] = new Person($autor, $instituicao);
+                }else if($no->tagName=='div'){
+                  if($no->getAttribute('class')=='tags mr-sm'){
+                    $type = $no->nodeValue;
+                    print_r($type . "\n");
+                  }else {
+                    $id = $no->nodeValue;
+                    print_r($id . "\n");
+                  }
+  
+                }
+                
+              }
+              
+            }
+        
+         
+        } 
+        $paper = new Paper($id,$title,$type,[$person]);
+        print_r($paper);
+        
+      } 
+    
+
+    }catch(\DOMException){
+      print("Erro");
+    }
+    
+  }
+  /**
+   * Summary of printTable
+   * @param mixed $id
+   * @param mixed $title
+   * @param mixed $type
+   * @param mixed $autor
+   * @param mixed $instituicao
+   * @return void
+   */
+  function printTable($id,$title,$type,$autor,$instituicao){
+        $filePath = 'exemplo.xlsx';
+        $reader = ReaderFactory::create(ReaderFactory::TYPE_XLSX);
+        $reader->open($filePath);
+        $writer = WriterEntityFactory::createXLSXWriter();
+        $writer->openToFile($filePath);
+
+        $style = (new StyleBuilder())
+          ->setFontBold()
+          ->build();
+
+        $headerRow = WriterEntityFactory::createRowFromArray(['ID','Title',	'Type',	'Author 1',	'Author 1 Institution',	'Author 2'	,'Author 2 Institution', 'Author 3','Author 3','Author 3 Institution'], $style);
+        $writer->addRow($headerRow); 
+        
+
+        $reader->each(function($row) use ($writer) {
+
+          $guid = $row[4]; // Assuma que o GUID está na coluna 5 (índice 4)
+      
+          if ($guid !== '') {
+      
+              // O GUID já existe, a cédula já foi escrita antes
+      
+              echo "Cédula já escrita.\n";
+      
+          } else {
+      
+              // Gere um novo GUID e escreva na cédula
+      
+              $newGuid = uniqid();
+      
+              $writer->addRow([
+      
+                  $row[0],
+      
+                  $row[1],
+      
+                  $row[2],
+      
+                  $row[3],
+      
+                  $newGuid,
+      
+              ]);
+      
+          }
+      
+      });
+      
+      
+      // Feche o leitor e o escritor
+      
+      $reader->close();
+        $teste = WriterEntityFactory::createRowFromArray([$id,$title,$type,$autor,$instituicao], $style);
+        $writer->addRow($teste);
+        $writer->close();
+
+        echo "Tabela criada com sucesso em $filePath \n" ;
   }
 
 }
