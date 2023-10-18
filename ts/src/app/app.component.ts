65122d7607d96bd071c67f6cc5c07b5d6064e0da
andressaribeiroo <andressaclecia.ribeiro@gmail.com>
2023-10-17 23:16:51 -0300

    feat: Declaração de propriedades a serem usadas

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -3,11 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'DevChuva';
   isExpanded: boolean = false;
+  isExpandedAnswered: boolean = false;
+  showComments: boolean = false;
+  showForm: boolean = false;
+  showIdeas = true;
+  showFeedback = false;
 
   toggleExpand() {
     this.isExpanded = !this.isExpanded;
