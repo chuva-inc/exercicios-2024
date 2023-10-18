7e63d51d58fd85ab4419e7c09580abbadf2e22b1
SuzuhRevv <joaopk04@hotmail.com>
2023-10-18 11:45:14 -0300

    Mudei a viewport do cypress para que ela se adequasse a minha tela porque um layout responsivo não foi especificado na documentação do exercicio, além disso fiz mudanças básicas no SCSS para que ficasse mais parecido com o protótipo do figma.

--- ts/cypress.config.ts
+++ ts/cypress.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
     runMode: 0,
     openMode: 0,
   },
-  viewportWidth: 1280,
-  viewportHeight: 800,
+  viewportWidth: 1440,
+  viewportHeight: 900,
   e2e: {
     setupNodeEvents(on, config) {
       getCompareSnapshotsPlugin(on, config);
