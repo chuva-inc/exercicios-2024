317265a5bc45926cc70e453a876e216e14835676
Gustavo Oliveira Viana <68523116+VianaGu@users.noreply.github.com>
2023-10-19 20:54:59 -0300

    feat:Escreve na tabela

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,23 +14,31 @@ class Scrapper {
   /**
    * Loads paper information from the HTML and returns the array with the data.
    */
+
   public function scrap(\DOMDocument $dom){
     $xpath = new \DOMXPath($dom);
     // Query the HTML for the paper cards.
     $paper_card = $xpath->query('.//a[@class="paper-card p-lg bd-gradient-left"]');
-    
     $filePath = 'exemplo.xlsx';
     $writer = WriterEntityFactory::createXLSXWriter();
     $writer->openToFile($filePath);
     $style = (new StyleBuilder())
       ->setFontBold()
       ->build();
-
+    $lineStyle = (new StyleBuilder())
+    ->setShouldWrapText(true)
+    ->build();  
     $headerRow = WriterEntityFactory::createRowFromArray(['ID','Title',	'Type',	'Author 1',
       'Author 1 Institution',	'Author 2'	,'Author 2 Institution', 'Author 3',
-      'Author 3 Institution','Author 4','Author 4 Institution'], $style);
+      'Author 3 Institution','Author 4','Author 4 Institution','Author 5','Author 5 Institution'
+      ,'Author 6','Author 6 Institution','Author 7','Author 7 Institution'
+      ,'Author 8','Author 8 Institution','Author 9','Author 9 Institution'], $style);
     $writer->addRow($headerRow); 
-   
+    $cells = [];
+
+    $id='';
+    $type='';
+    $title='';
     try{
       // Iterate over the paper cards.
       foreach ($paper_card as $elemento) {
@@ -50,45 +58,68 @@ class Scrapper {
             $count = 1;
             foreach ($filhoNo as $no){
               // If the tag name is 'div', then it is the title of the paper.    
-              if($no->nodeName == 'span'){
-                $instituicao = $no->getAttribute('title');
-                print_r("Instituição: $instituicao \n");    
-                $autor = $no->textContent;
-                print_r("Autores: $autor \n");
-                if($count == 1){
-                  $row = WriterEntityFactory::createRowFromArray(['','','',$autor,$instituicao]);
-                }elseif($count == 2){
-                  $row = WriterEntityFactory::createRowFromArray(['','','','','',$autor,$instituicao]);
-                }elseif($count == 3){
-                  $row = WriterEntityFactory::createRowFromArray(['','','','','','','',$autor,$instituicao]);
-                }elseif($count == 4){
-                  $row = WriterEntityFactory::createRowFromArray(['','','','','','','','','',$autor,$instituicao]);
-                }
-                $writer->addRow($row);
-                $count++;
-              }else if($no->nodeName == 'div'){
+              if($no->nodeName == 'div'){
                 if($no->getAttribute('class') == 'tags mr-sm'){
                   $type = $no->nodeValue;
                   print_r($type . "\n");
                 }else{
                   $id = $no->nodeValue;
                   print_r($id . "\n");
-                  $row = WriterEntityFactory::createRowFromArray([$id,$title,$type]);
-                  $writer->addRow($row);
                 }
 
+              }elseif($no->nodeName == 'span'){
+                $instituicao = $no->getAttribute('title');
+                print_r("Instituição: $instituicao \n");    
+                $autor = $no->textContent;
+                print_r("Autores: $autor \n");
+                if($count == 1){
+                  $cells = [
+                    WriterEntityFactory::createCell($id),
+                    WriterEntityFactory::createCell($title),
+                    WriterEntityFactory::createCell($type),
+                    WriterEntityFactory::createCell($autor),
+                    WriterEntityFactory::createCell($instituicao),
+                  ];
+                }elseif($count == 2){
+                  $cells[] = WriterEntityFactory::createCell($autor);
+                  $cells[] = WriterEntityFactory::createCell($instituicao);
+                }elseif($count == 3){
+                  $cells[] = WriterEntityFactory::createCell($autor);
+                  $cells[] = WriterEntityFactory::createCell($instituicao);
+                }elseif($count == 4){
+                  $cells[] = WriterEntityFactory::createCell($autor);
+                  $cells[] = WriterEntityFactory::createCell($instituicao);
+                }elseif($count == 4){
+                  $cells[] = WriterEntityFactory::createCell($autor);
+                  $cells[] = WriterEntityFactory::createCell($instituicao);
+                }elseif($count == 5){
+                  $cells[] = WriterEntityFactory::createCell($autor);
+                  $cells[] = WriterEntityFactory::createCell($instituicao);
+                }elseif($count == 6){
+                  $cells[] = WriterEntityFactory::createCell($autor);
+                  $cells[] = WriterEntityFactory::createCell($instituicao);
+                }elseif($count == 7){
+                  $cells[] = WriterEntityFactory::createCell($autor);
+                  $cells[] = WriterEntityFactory::createCell($instituicao);
+                }elseif($count == 8){
+                  $cells[] = WriterEntityFactory::createCell($autor);
+                  $cells[] = WriterEntityFactory::createCell($instituicao);
+                }elseif($count == 9){
+                  $cells[] = WriterEntityFactory::createCell($autor);
+                  $cells[] = WriterEntityFactory::createCell($instituicao);
+                }
+                $count++;
               }
               
             }
             
           }
-          
-         
+               
         } 
-        
+        $singleRow = WriterEntityFactory::createRow($cells,$lineStyle);
+        $writer->addRow($singleRow);  
       } 
       echo "Tabela criada com sucesso em $filePath \n" ;
-
     }catch(\DOMException $e){
       print_r($e->getMessage());
      }
