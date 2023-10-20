687e85ba819f2e25da01dd679056ec97b8425afa
Gustavo Oliveira Viana <68523116+VianaGu@users.noreply.github.com>
2023-10-20 08:18:23 -0300

    lapidando scrapper

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -27,19 +27,13 @@ class Scrapper {
       ->setShouldWrapText(TRUE)
       ->build();
     $headerRow = WriterEntityFactory::createRowFromArray([
-      'ID', 'Title', 'Type', 'Author 1', 'Author 1 Institution',	'Author 2', 
       'Author 2 Institution', 'Author 3', 'Author 3 Institution', 'Author 4', 
       'Author 4 Institution', 'Author 5', 'Author 5 Institution', 'Author 6', 
       'Author 6 Institution', 'Author 7', 'Author 7 Institution', 'Author 8', 
       'Author 8 Institution', 'Author 9', 'Author 9 Institution', 
     ], $style);
     $writer->addRow($headerRow); 
-    $cells = [
-      
-    ];
-
-    $id = '';
-    $type = '';
+    $cells = [ ];
     $title = '';
     try {
       // Iterate over the paper cards.
