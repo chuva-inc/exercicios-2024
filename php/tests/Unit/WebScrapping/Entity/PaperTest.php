3b01fde4dca55ef2c347a59fd78a333b7f6c0157
jonathanluis2022 <jonathan.luisrodrigues@hotmail.com>
2023-10-22 16:13:23 -0300

    criei o  projeto apartir do fork antes eu tinha só clonado

--- php/tests/Unit/WebScrapping/Entity/PaperTest.php
+++ php/tests/Unit/WebScrapping/Entity/PaperTest.php
@@ -6,6 +6,8 @@ use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
 use PHPUnit\Framework\TestCase;
 
+// require_once 'php/vendor/autoload.php';
+
 /**
  * Tests requirements for Paper.
  */
@@ -15,15 +17,15 @@ class PaperTest extends TestCase {
    * Tests construct().
    */
   public function testConstruct() {
-    $paper = new Paper(123, 'Paper title', 'Oral presentation', [
-      new Person('Evariste Galois', 'Lycée Louis-le-Grand'),
+    $paper = new Paper(3030, 'jonathan luis', 'Honore tournieux', [
+      new Person('x6', 'drycka'),
     ]);
 
-    $this->assertEquals(123, $paper->id);
-    $this->assertEquals('Paper title', $paper->title);
-    $this->assertEquals('Oral presentation', $paper->type);
-    $this->assertEquals('Evariste Galois', $paper->authors[0]->name);
-    $this->assertEquals('Lycée Louis-le-Grand', $paper->authors[0]->institution);
+    $this->assertEquals(3030, $paper->id);
+    $this->assertEquals('jonathan luis', $paper->title);
+    $this->assertEquals('Honore tournieux', $paper->type);
+    $this->assertEquals('x6', $paper->authors[0]->name);
+    $this->assertEquals('drycka', $paper->authors[0]->institution);
   }
 
 }
