e2e9ac5521e0188a7578b273e046dc09b4602de5
AzvedoGabriel <contato.gabrielazevedo0@gmail.com>
2023-11-04 19:12:29 -0300

    Fix: modifications to run the tests

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -5,64 +5,44 @@ namespace Chuva\Php\WebScrapping\Entity;
 /**
  * The Paper class represents the row of the parsed data.
  */
-class Paper {
+class Paper
+{
+    /**
+     * Paper Id.
+     *
+     * @var int
+     */
+    public $id;
 
-  /**
-   * Paper Id.
-   *
-   * @var int
-   */
-  public int $id;
+    /**
+     * Paper Title.
+     *
+     * @var string
+     */
+    public $title;
 
-  /**
-   * Paper Title.
-   *
-   * @var string
-   */
-  public string $title;
+    /**
+     * The paper type (e.g. Poster, Nobel Prize, etc).
+     *
+     * @var string
+     */
+    public $type;
 
-  /**
-   * The paper type (e.g. Poster, Nobel Prize, etc).
-   *
-   * @var string
-   */
-  public string $type;
+    /**
+     * Paper authors.
+     *
+     * @var \Chuva\Php\WebScrapping\Entity\Person[]
+     */
+    public $authors;
 
-  /**
-   * Paper authors.
-   *
-   * @var Person[]
-   */
-  public array $authors;
-
-  /**
-   * Builder.
-   */
-  public function __construct($id, $title, $type, $authors = []) {
-      $this->id = $id;
-      $this->title = $title;
-      $this->type = $type;
-      $this->authors = $authors;
-  }
-
-    public function getId(): int
+    /**
+     * Builder.
+     */
+    public function __construct($id, $title, $type, $authors = [])
     {
-        return $this->id;
+        $this->id = $id;
+        $this->title = $title;
+        $this->type = $type;
+        $this->authors = $authors;
     }
-
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
-
-    public function getType(): string
-    {
-        return $this->type;
-    }
-
-    public function getAuthors(): array
-    {
-        return $this->authors;
-    }
-
 }
