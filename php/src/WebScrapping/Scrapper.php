ff1a7b09406de3157d41ab9923300c2c78fa0840
AzvedoGabriel <contato.gabrielazevedo0@gmail.com>
2023-11-04 17:53:03 -0300

    Fix: delete unnecessary comments

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -8,23 +8,48 @@ use Chuva\Php\WebScrapping\Entity\Person;
 /**
  * Does the scrapping of a webpage.
  */
-class Scrapper {
+class Scrapper
+{
+    /**
+     * Loads paper information from the HTML and returns the array with the data.
+     */
+    public function scrap(\DOMDocument $dom): array
+    {
+        $xPath = new \DOMXPath($dom);
 
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
-  public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
-  }
+        $containerArticles = $xPath->query("//a[@class='paper-card p-lg bd-gradient-left']");
 
+        $results = array();
+
+        foreach ($containerArticles as $article) {
+            $id = $xPath->query(".//div[@class='tags flex-row mr-sm']/div[@class='volume-info']", $article)->item(0)->nodeValue;
+            $title = $xPath->query(".//h4[@class='my-xs paper-title']", $article)->item(0)->nodeValue;
+            $tags = $xPath->query(".//div[@class='tags mr-sm']", $article)->item(0)->nodeValue;
+            $authors = $this->getAuthors($xPath->query(".//div[@class='authors']/span[@title]", $article));
+
+            $paper = new Paper($id, $title, $tags, $authors);
+            $results[] = $paper;
+        }
+
+        return $results;
+    }
+
+    private function getAuthors(\DOMNodeList $nodes): array
+    {
+        $authors = array();
+
+        foreach ($nodes as $node) {
+            $name = $node->nodeValue;
+            $title = $node->getAttribute('title');
+            $author = new Person($name, $title);
+            $authors[] = $author;
+        }
+
+        return $authors;
+    }
 }
+
+
+
+
+
