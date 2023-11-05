48de41a27e2fe2e405c74664036234244c77e566
AzvedoGabriel <contato.gabrielazevedo0@gmail.com>
2023-11-04 21:34:58 -0300

    Feat: implementação da lógica para o csv no `Main`

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,22 +2,65 @@
 
 namespace Chuva\Php\WebScrapping;
 
+use OpenSpout\Common\Entity\Style\CellAlignment;
+use OpenSpout\Writer\Common\Creator\Style\StyleBuilder;
+use OpenSpout\Writer\Common\Creator\WriterEntityFactory;
+
 /**
  * Runner for the Webscrapping exercice.
  */
 class Main {
 
-  /**
-   * Main runner, instantiates a Scrapper and runs.
-   */
   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
-
+    @$dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
     $data = (new Scrapper())->scrap($dom);
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
+    $csvPath = __DIR__.'/../../assets/teste.xlsx';
+    $writer = WriterEntityFactory::createCSVWriter();
+    $writer->openToFile($csvPath);
+
+    $style = (new StyleBuilder())
+      ->setFontName('Arial')
+      ->setFontSize(11)
+      ->setFontBold()
+      ->setCellAlignment(CellAlignment::RIGHT)
+      ->build();
+
+    $headers = ['ID', 'Title', 'Type'];
+    for ($i = 0; $i < 17; $i++) {
+      $headers[] = "Author $i";
+      $headers[] = "Author $i Institution";
+    }
+    $rowFromHeaders = WriterEntityFactory::createRowFromArray($headers);
+    $writer->addRow($rowFromHeaders);
+
+    foreach ($data as $paper){
+      $rowData = [
+        $paper->id,
+        $paper->title,
+        $paper->type,
+      ];
+
+      $authors = array_slice($paper->authors, 0, 17);
+
+      for ($i = 0; $i < 17; ++$i) {
+        if (isset($authors[$i])) {
+          $author = $authors[$i];
+          $rowData[] = $author->name;
+          $rowData[] = $author->institution;
+        } else {
+          // Fill in blank values if there is no corresponding author
+          $rowData[] = '';
+          $rowData[] = '';
+        }
+      }
+      $row = WriterEntityFactory::createRowFromArray($rowData);
+      $row->setStyle($style);
+      $writer->addRow($row);
+    }
+    $writer->close();
+
   }
 
 }
