af22cd88511b62c61ef3b4588ab33a4dcfc48ada
AzvedoGabriel <contato.gabrielazevedo0@gmail.com>
2023-11-04 21:57:57 -0300

    Refactor: Modificações para os testes do actions

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -12,70 +12,61 @@ use OpenSpout\Writer\Common\Creator\WriterEntityFactory;
 class Main
 {
 
-    /**
-     * Runner for the Webscrapping exercice.
-     */
-    public static function run(): void
-    {
-        $dom = new \DOMDocument('1.0', 'utf-8');
-        @$dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+  /**
+   * Runner for the Webscrapping exercice.
+   */
+  public static function run(): void
+  {
+    $dom = new \DOMDocument('1.0', 'utf-8');
+    @$dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-        $data = (new Scrapper())->scrap($dom);
+    $data = (new Scrapper())->scrap($dom);
 
-        $csvPath = (__DIR__ . '/../../assets/teste.csv');
+    $csvPath = (__DIR__ . '/../../assets/teste.csv');
 
-        $writer = WriterEntityFactory::createCSVWriter();
-        $writer->openToFile($csvPath);
+    $writer = WriterEntityFactory::createCSVWriter();
+    $writer->openToFile($csvPath);
 
-        $style = (new StyleBuilder())
-            ->setFontName('Arial')
-            ->setFontSize(11)
-            ->setFontBold()
-            ->setCellAlignment(CellAlignment::RIGHT)
-            ->build();
+    $style = (new StyleBuilder())->setFontName('Arial')->setFontSize(11)->setFontBold()->setCellAlignment(CellAlignment::RIGHT)->build();
 
-        $headers = ['ID', 'Title', 'Type'];
-        for ($i = 0; $i < 17; $i++) {
-            $headers[] = "Author $i";
-            $headers[] = "Author $i Institution";
-        }
-
-        $rowFromHeaders = WriterEntityFactory::createRowFromArray($headers);
-        $writer->addRow($rowFromHeaders);
-
-        foreach ($data as $paper) {
-
-            $rowData = [
-            $paper->id,
-            $paper->title,
-            $paper->type,
-            ];
-
-            $authors = array_slice($paper->authors, 0, 17);
-
-            for ($i = 0; $i < 17; ++$i) {
-                if (isset($authors[$i])) {
-
-                    $author = $authors[$i];
-
-                    $rowData[] = $author->name;
-
-                    $rowData[] = $author->institution;
-
-                } else {
-
-                    // Fill in blank values if there is no corresponding author.
-                    $rowData[] = '';
-
-                    $rowData[] = '';
-                }
-            }
-            $row = WriterEntityFactory::createRowFromArray($rowData);
-            $row->setStyle($style);
-            $writer->addRow($row);
-        }
-        $writer->close();
-
+    $headers = ['ID', 'Title', 'Type'];
+    for ($i = 0; $i < 17; $i++) {
+      $headers[] = "Author $i";
+      $headers[] = "Author $i Institution";
     }
+
+    $rowFromHeaders = WriterEntityFactory::createRowFromArray($headers);
+    $writer->addRow($rowFromHeaders);
+
+    foreach ($data as $paper) {
+
+      $rowData = [$paper->id, $paper->title, $paper->type,];
+
+      $authors = array_slice($paper->authors, 0, 17);
+
+      for ($i = 0; $i < 17; ++$i) {
+        if (isset($authors[$i])) {
+
+          $author = $authors[$i];
+
+          $rowData[] = $author->name;
+
+          $rowData[] = $author->institution;
+
+        } else {
+
+          // Fill in blank values if there is no corresponding author.
+          $rowData[] = '';
+
+          $rowData[] = '';
+        }
+      }
+      $row = WriterEntityFactory::createRowFromArray($rowData);
+      $row->setStyle($style);
+      $writer->addRow($row);
+    }
+    $writer->close();
+
+  }
 
 }
