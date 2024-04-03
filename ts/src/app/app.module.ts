4d245c17e917ba463de215f0b54e4ee7b1f6ccd9
Lucas Fernandes <lucasfeitosa8@gmail.com>
2024-04-03 10:28:44 -0300

    (feat): Positioned page footer and start development of the main content section

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LeftSideNavMenuComponent } from './components/left-side-nav-menu/left-side-nav-menu.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { PageMainContentComponent } from './components/page-main-content/page-main-content.component';
+import { PageFooterComponent } from './components/page-footer/page-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LeftSideNavMenuComponent,
     PageHeaderComponent,
-    PageMainContentComponent
+    PageMainContentComponent,
+    PageFooterComponent
   ],
   imports: [
     BrowserModule
