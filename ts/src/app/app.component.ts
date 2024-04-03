5c8f13777cd3a0d150d842fd8734ccb6eb8457f3
Luciano Lins <luciano_lins1@hotmail.com>
2024-04-02 23:29:52 -0300

    feat: installing dependencies, creating structure

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './components/app.component.html',
+  styleUrls: ['./app.component.scss', '../styles.scss']
 })
 export class AppComponent {
   title = 'DevChuva';
+  logoimage:string = './components/assets/logo.png'
 }
