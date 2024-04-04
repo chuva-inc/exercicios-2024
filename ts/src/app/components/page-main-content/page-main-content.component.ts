616ed8d53df750de5f373871535b193e820353b5
Lucas Fernandes <lucasfeitosa8@gmail.com>
2024-04-04 15:04:33 -0300

    (feat): Create new discuss topic button is working and form is appearing and styled

--- ts/src/app/components/page-main-content/page-main-content.component.ts
+++ ts/src/app/components/page-main-content/page-main-content.component.ts
@@ -12,4 +12,34 @@ export class PageMainContentComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showForm() {
+    let newForm = document.querySelector(".discuss-content");
+
+    if(newForm) {
+      newForm.innerHTML = `
+      <div class="create-new-topic-wrapper">
+        <span class="create-new-topic-title">
+          Tem uma dúvida ou sugestão? Compartilhe seu feedback com os autores!
+        </span>
+
+        <div class="topic-subject-wrapper">
+          <span class="new-topic-subject">Assunto</span>
+            <br>
+          <input type="text" name="newSubjectTitle" id="newSubjectTitle">
+        </div>
+        
+        <div class="topic-content-wrapper">
+          <span class="new-topic-content">Conteúdo</span>
+            <br>
+          <input type="text" name="newSubjectContent" id="newSubjectContent">
+
+          <div class="submit-subject-button-wrapper">
+            <div class="subject-submit-button">
+              <p id="submit-subject">Enviar<p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    }
+  } 
 }
