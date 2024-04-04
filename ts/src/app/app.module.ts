ec4f2fd4e977b5b10494b03763c2cc2abb5ad871
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2024-04-04 20:04:06 -0300

    feat: cria seção de resumo(sem expansão)

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AsideMenuComponent } from './components/aside-menu/aside-menu.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AsideMenuComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule
