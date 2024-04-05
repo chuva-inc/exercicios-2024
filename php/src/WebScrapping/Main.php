0e65854153c6d514cb511332ca3b6875c1e3f994
folasPuc <rafael.rsato@gmail.com>
2024-04-05 16:36:31 -0300

    Resolução do exercício PHP Chuva 2024
    Resolução feita por Rafael Rodrigues Sato do exercício de PHP.

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Chuva\Php\WebScrapping;
-
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
 /**
  * Runner for the Webscrapping exercice.
  */
@@ -12,12 +13,55 @@ class Main {
    */
   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
+    libxml_use_internal_errors(true);
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
-
     $data = (new Scrapper())->scrap($dom);
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
+    $writer = WriterEntityFactory::createXLSXWriter();
+
+    $filePath = 'D:\Users\Rafael\Documents\GitHub\exercicios-2024\php\src\WebScrapping\output.xlsx';
+
+    $writer->openToFile($filePath);
+  
+  $writer->addRow(WriterEntityFactory::createRowFromArray([
+    'ID', 'Title', 'Type',
+    'Author 1', 'Author 1 Instituition',
+    'Author 2', 'Author 2 Instituition',
+    'Author 3', 'Author 3 Instituition',
+    'Author 4', 'Author 4 Instituition',
+    'Author 5', 'Author 5 Instituition',
+    'Author 6', 'Author 6 Instituition',
+    'Author 7', 'Author 7 Instituition',
+    'Author 8', 'Author 8 Instituition',
+    'Author 9', 'Author 9 Instituition',
+  ]));
+
+  //Iterando sobre os dados e adicionando-os à planilha
+  foreach ($data[0] as $trabalho) {
+    $id = $trabalho[0];
+    $titulo = $trabalho[1];
+    $tipo = $trabalho[2];
+    $autores = array_slice($trabalho, 3, count($trabalho) - 1);
+
+  //Array para armazenar todos os autores e instituições
+    $autoresEInstituicoes = [];
+
+  //Iterando sobre os autores e suas instituições em pares
+  for ($i = 0; $i < count($autores); $i += 2) {
+      $autor = $autores[$i];
+      $instituicao = $autores[$i + 1];
+      $autoresEInstituicoes[] = $autor;
+      $autoresEInstituicoes[] = $instituicao;
+  }
+
+  // Adicionando uma linha com os dados do trabalho atual
+  $rowData = array_merge([$id, $titulo, $tipo], $autoresEInstituicoes);
+  $writer->addRow(WriterEntityFactory::createRowFromArray($rowData));
+}
+$writer->close();
+    echo 'Excel criado com sucesso!';
   }
 
 }
+
+?>
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,16 +14,105 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
+
+    $a_elements = $dom->getElementsByTagName('a');
+    $data_title = [];
+    $data_authors = [];
+    $data_id = [];
+    $data_type =[];
+    $data_inst = [];
+    $num_integrantes_grupo = [];
+    $inicio = 0;
+
+    //Retorna o titulo dos artigos
+    foreach ($a_elements as $a_element) {
+      $h4_elements = $a_element->getElementsByTagName('h4');
+      foreach ($h4_elements as $h4_element) {
+        $data_title[] = $h4_element->textContent;
+      }
+    }
+
+    //Retorna os nomes dos authores_separados
+    foreach ($a_elements as $a_element) {
+      $div_elements = $a_element->getElementsByTagName('div');
+      foreach ($div_elements as $div_element) {
+        if ($div_element->getAttribute('class') === 'authors') {
+            $span_elements = $div_element->getElementsByTagName('span');
+            foreach ($span_elements as $span_element) {
+              $data_authors[] = $span_element->textContent;
+          }
+        }
+      }
+    }
+
+    //Retorna a quantidade de membros por grupo
+    foreach ($a_elements as $a_element) {
+      $div_elements = $a_element->getElementsByTagName('div');
+      foreach ($div_elements as $div_element) {
+        if ($div_element->getAttribute('class') === 'authors') {
+            $intr_1 = $div_element->textContent;
+            $intr_2 = explode(';', $intr_1);
+            $number_author_grupo = count($intr_2);
+            $number_author_grupo = $number_author_grupo - 1;
+            $num_integrantes_grupo[] = $number_author_grupo;
+            
+        }
+      }
+    }
+    //Retorna os ids dos artigos
+    foreach ($a_elements as $a_element) {
+      $div_elements = $a_element->getElementsByTagName('div');
+      foreach ($div_elements as $div_element) {
+        if ($div_element->getAttribute('class') === 'tags flex-row mr-sm') {
+            $data_id[] = $div_element->textContent;
+        }
+      }
+    }
+
+    //Retorna os tipos dos arquivos
+    foreach ($a_elements as $a_element) {
+      $div_elements = $a_element->getElementsByTagName('div');
+      foreach ($div_elements as $div_element) {
+        if ($div_element->getAttribute('class') === 'tags mr-sm') {
+            $data_type[] = $div_element->textContent;
+        }
+      }
+    }
+
+
+    //Retorna os nomes das instituicoes
+    foreach ($a_elements as $a_element) {
+      $div_elements = $a_element->getElementsByTagName('div');
+      foreach ($div_elements as $div_element) {
+        if ($div_element->getAttribute('class') === 'authors') {
+            $span_elements = $div_element->getElementsByTagName('span');
+            foreach ($span_elements as $span_element) {
+              if ($span_element->hasAttribute('title')) {
+                $title_value = $span_element->getAttribute('title');
+                $data_inst[] = $title_value;
+            }
+          }
+        }
+      }
+    }
+
+    $work_counter = count($data_id);
+    $authors_counter = count($data_authors);
+    $data_final = [];
+    //Montando o array final
+    for($i = 0; $i < $work_counter; $i++){
+      $data_final[$i] = [];
+      //id, titulo e tipo do trabalho
+      array_push($data_final[$i], $data_id[$i], $data_title[$i], $data_type[$i]);
+      //for que adiciona os autores e suas instituicoes em seus devidos trabalhos
+      for($j = $inicio; $j < $inicio + $num_integrantes_grupo[$i]; $j++){
+        array_push($data_final[$i], $data_authors[$j], $data_inst[$j]);
+      }
+      $inicio += $num_integrantes_grupo[$i];
+
+    }
     return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
+      $data_final
     ];
   }
 
