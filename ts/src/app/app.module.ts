cb8e9573d102342147cb70316088e7a081545226
mdiasfran <contate.afran@gmail.com>
2024-04-05 00:37:04 -0300

    Cria conteúdo principal

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
+import { AsideComponent } from './components/content/aside/aside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule
---
+++ ts/src/app/components/content/aside/aside.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aside',
+  templateUrl: './aside.component.html',
+  styleUrls: ['./aside.component.scss']
+})
+export class AsideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
