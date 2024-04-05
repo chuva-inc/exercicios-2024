c963fb3d6465835f56a62196f57eada046b314d3
Lucas Fernandes <lucasfeitosa8@gmail.com>
2024-04-05 15:23:08 -0300

    (feat): Created and styled form for submit new subject and make logic for create new topic button to show form

--- ts/src/app/components/page-main-content/page-main-content.component.ts
+++ ts/src/app/components/page-main-content/page-main-content.component.ts
@@ -13,33 +13,10 @@ export class PageMainContentComponent implements OnInit {
   }
 
   showForm() {
-    let newForm = document.querySelector(".discuss-content");
+    let shareIdeas = document.querySelector(".share-ideas") as HTMLElement;
+    let newForm = document.querySelector(".create-new-topic-wrapper") as HTMLElement;
 
-    if(newForm) {
-      newForm.innerHTML = `
-      <div class="create-new-topic-wrapper">
-        <span class="create-new-topic-title">
-          Tem uma dúvida ou sugestão? Compartilhe seu feedback com os autores!
-        </span>
-
-        <div class="topic-subject-wrapper">
-          <span class="new-topic-subject">Assunto</span>
-            <br>
-          <input type="text" name="newSubjectTitle" id="newSubjectTitle">
-        </div>
-        
-        <div class="topic-content-wrapper">
-          <span class="new-topic-content">Conteúdo</span>
-            <br>
-          <input type="text" name="newSubjectContent" id="newSubjectContent">
-
-          <div class="submit-subject-button-wrapper">
-            <div class="subject-submit-button">
-              <p id="submit-subject">Enviar<p>
-            </div>
-          </div>
-        </div>
-      </div>`;
-    }
+    shareIdeas.style.display = 'block' ? 'none' : 'block';
+    newForm.style.display = 'none' ? 'block' : 'none';
   } 
 }
