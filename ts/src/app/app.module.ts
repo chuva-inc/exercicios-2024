5726d0f680efb71f7e328bc3523bb6e9d8897928
mdiasfran <contate.afran@gmail.com>
2024-04-04 22:51:26 -0300

    Cria header

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
