78304784e54e415fbed346920947d2055f1a85f7
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-04-06 17:14:05 -0300

    feat: add interativeness in text view

--- ts/src/app/components/summary/summary.component.ts
+++ ts/src/app/components/summary/summary.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.scss']
+  styleUrls: ['./summary.component.scss'],
 })
 export class SummaryComponent implements OnInit {
+  viewAllText: boolean = false;
+  removeText: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showAllText(): void {
+    this.viewAllText = true;
+
+    if (this.viewAllText) {
+      this.removeText = true;
+    }
   }
-
 }
