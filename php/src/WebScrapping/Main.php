b7fb6e90226e70c2cd422ba178067aeb10e1f44d
André Luís Bianchini Filho <albianchinifilho@hotmail.com>
2024-04-08 20:06:00 -0300

    Resolução Atividade PHP
    Resolução feita por André Luís Bianchini Filho

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,22 +2,49 @@
 
 namespace Chuva\Php\WebScrapping;
 
-/**
- * Runner for the Webscrapping exercice.
- */
-class Main {
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use DOMDocument;
 
-  /**
-   * Main runner, instantiates a Scrapper and runs.
-   */
-  public static function run(): void {
-    $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+class Main{
+    //o Caminho para a planilhar ser criada.
+    private const OUTPUT_FILE_PATH = 'C:\Users\Andre\Documents\GitHub\exercicios-2024\php\assets\PlanilhaFinal.xlsx';
 
-    $data = (new Scrapper())->scrap($dom);
+    //Cabeçalho que vai ser posto na planilha.
+    private const HEADERS = [
+        'ID', 'Titulo', 'Tipo',
+        'Autor 1', 'Autor 1 Instituição', 'Autor 2', 'Autor 2 Instituição',
+        'Autor 3', 'Autor 3 Instituição', 'Autor 4', 'Autor 4 Instituição',
+        'Autor 5', 'Autor 5 Instituição', 'Autor 6', 'Autor 6 Instituição',
+        'Autor 7', 'Autor 7 Instituição', 'Autor 8', 'Autor 8 Instituição',
+        'Autor 9', 'Autor 9 Instituição', 'Autor 10', 'Autor 10 Instituição',
+    ];
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
-  }
+    //Define o método run, que carrega o documento HTML, executando o método scrap da classe Scrapper
+    // para extrair os dados e cria a planilha.
+    public static function run(): void{
+        $dom = new DOMDocument('1.0', 'utf-8');
+        libxml_use_internal_errors(true);
+        $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
+        //extrai os dados do HTML.
+        $scrapper = new Scrapper();
+        $data = $scrapper->scrap($dom);
+
+        //Chama o método createXlsxFile para criar a planilha com os dados extraídos.
+        self::createXlsxFile($data);
+    }
+
+    // Cria um escritor de planilha, abre o arquivo de saída, adiciona os cabeçalhos e
+    // itera sobre os dados, adicionando-os à planilha.
+    private static function createXlsxFile(array $data): void{
+        $writer = WriterEntityFactory::createXLSXWriter();
+        $writer->openToFile(self::OUTPUT_FILE_PATH);
+        $writer->addRow(WriterEntityFactory::createRowFromArray(self::HEADERS));
+
+        foreach ($data[0] as $articleData) {
+            $writer->addRow(WriterEntityFactory::createRowFromArray($articleData));
+        }
+
+        $writer->close();
+    }
 }
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -2,29 +2,43 @@
 
 namespace Chuva\Php\WebScrapping;
 
-use Chuva\Php\WebScrapping\Entity\Paper;
-use Chuva\Php\WebScrapping\Entity\Person;
+use DOMDocument;
+use DOMXPath;
 
-/**
- * Does the scrapping of a webpage.
- */
-class Scrapper {
+class Scrapper{
+    public function scrap(DOMDocument $dom): array{
+        $xpath = new DOMXPath($dom);
+        $articlesData = [];
 
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
-  public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
-  }
+        //Executa uma consulta XPath para encontrar todos os elementos <a> com a classe 'paper-card'
+        $articles = $xpath->query("//a[contains(@class,'paper-card')]");
 
+        //Para cada artigo, chama o método extractArticleData para extrair os dados específicos do artigo.
+        foreach ($articles as $article) {
+            $articlesData[] = $this->extractArticleData($xpath, $article);
+        }
+
+        //Retorna o array com os dados dos artigos
+        return [$articlesData];
+    }
+
+    //O extractArticleData e as consultas XPath são ajustados para extrair os dados baseados na estruturas do HTML.
+    private function extractArticleData(DOMXPath $xpath, $article): array{
+        $title = $xpath->query(".//h4[contains(@class,'paper-title')]", $article)->item(0)->nodeValue;
+        $authorsList = $xpath->query(".//div[contains(@class,'authors')]/span", $article);
+        $presentationType = $xpath->query(".//div[contains(@class,'tags')][1]", $article)->item(0)->nodeValue;
+        $id = $xpath->query(".//div[contains(@class,'volume-info')]", $article)->item(0)->nodeValue;
+
+        //Pego o nome dos autores e da instituiçao em arrays separados para No final, combinamos
+        //o ID, título, tipo de apresentação, autores e instituições em um único array e o retornamos.
+        $authors = [];
+        $institutions = [];
+        foreach ($authorsList as $author) {
+            $authors[] = trim(explode(";", $author->nodeValue)[0]);
+            $institutions[] = trim($author->getAttribute('title'));
+        }
+
+        //Aqui é retornado o unico array.
+        return array_merge([$id, $title, $presentationType], $authors, $institutions);
+    }
 }
