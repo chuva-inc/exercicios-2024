e0242778cb5168230b466dab1f1c5ada11eaddbe
AdrielSantana <144896671+wDAdriel@users.noreply.github.com>
2024-04-10 23:09:21 -0300

    Criado a classe openspout para utilizar a bibilioteca e criado a logica para salvar os dados extraidos da pagina html

--- php/src/WebScrapping/OpenSpout.php
+++ php/src/WebScrapping/OpenSpout.php
@@ -3,8 +3,6 @@
 namespace Chuva\Php\WebScrapping;
 
 use OpenSpout\Writer\Common\Creator\WriterEntityFactory;
-use OpenSpout\Common\Entity\Row;
-use OpenSpout\Reader\Common\Creator\ReaderEntityFactory;
 
 class OpenSpout
 {
@@ -12,37 +10,53 @@ class OpenSpout
     {
         $writer = WriterEntityFactory::createXLSXWriter();
 
-        $writer->openToFile(__DIR__ . '/../../assets/model1.xls'); // write data to a file or to a PHP stream
-        //$writer->openToBrowser($fileName); // stream data directly to the browser
+        $writer->openToFile(__DIR__ . '/../../assets/model-new.xls'); // write data to a file or to a PHP stream
 
-        $cells = [
-            WriterEntityFactory::createCell('Carl'),
-            WriterEntityFactory::createCell('is'),
-            WriterEntityFactory::createCell('great!'),
+        $head = [
+            WriterEntityFactory::createCell('ID'),
+            WriterEntityFactory::createCell('Title'),
+            WriterEntityFactory::createCell('Type'),
         ];
 
-        $cells = [
-            WriterEntityFactory::createCell('Carl'),
-            WriterEntityFactory::createCell('is'),
-            WriterEntityFactory::createCell('great!'),
-        ];
-
+        for( $i = 1; $i <= $this->countAuthorsMax($data); $i++) {
+            array_push($head, WriterEntityFactory::createCell('Author '. $i));
+            array_push($head, WriterEntityFactory::createCell('Author '. $i .' Institution'));
+        }
         /** add a row at a time */
-        $singleRow = WriterEntityFactory::createRow($cells);
+        $singleRow = WriterEntityFactory::createRow($head);
         $writer->addRow($singleRow);
 
-        /** add multiple rows at a time */
-        $multipleRows = [
-            WriterEntityFactory::createRow($cells),
-            WriterEntityFactory::createRow($cells),
-        ];
-        $writer->addRows($multipleRows);
+        foreach($data as $values) {
+            $paper = (array) $values;
+            $rowValue = [
+                $paper['id'],
+                $paper['title'],
+                $paper['type'],
+            ];
+            
+            foreach ($paper['authors'] as $author) {
+                array_push($rowValue, $author->name);
+                array_push($rowValue, $author->institution);
+            }
 
-        /** Shortcut: add a row from an array of values */
-        $values = ['Carl', 'is', 'great!'];
-        $rowFromValues = WriterEntityFactory::createRowFromArray($values);
-        $writer->addRow($rowFromValues);
+            $rowFromValues = WriterEntityFactory::createRowFromArray($rowValue);
+            $writer->addRow($rowFromValues);
+        }
 
         $writer->close();
+    }
+
+    public function countAuthorsMax(array $papers)
+    {
+        $count = 0;
+
+        foreach($papers as $paper) {
+            $countAuthors = count($paper->authors);
+            if ($countAuthors >= $count) {
+                $count = $countAuthors;
+            }
+        }
+
+        return $count;
     }
 }
