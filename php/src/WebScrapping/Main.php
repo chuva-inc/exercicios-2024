d55c1f159a400044c2f88f0ea4b5896d68e400c3
André Lucas <andre.lucas.mendes01@aluno.ifce.edu.br>
2024-04-12 12:28:54 -0300

    feature: Ignora PHP Warnings causados pelo DOMDocument

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -11,6 +11,7 @@ class Main {
    * Main runner, instantiates a Scrapper and runs.
    */
   public static function run(): void {
+    libxml_use_internal_errors(true);
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
