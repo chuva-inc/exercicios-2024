24c71c2c55108619ad95ed249e04b6152d4e77a1
Estevan Hernandes Silvade Oliveira <estevanhernandes1@gmail.com>
2024-04-11 21:33:49 -0300

    chore: criação dos componentes iniciais como "Navbar, Header e Content" para o layout da página

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarModule } from './components/navbar/navbar.module';
+import { HeaderModule } from './components/header/header.module';
+import { ContentModule } from './components/content/content.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NavbarModule, HeaderModule, ContentModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ ts/src/app/components/content/content.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContentComponent } from './content.component';
+
+@NgModule({
+  declarations: [ContentComponent],
+  imports: [CommonModule],
+  exports: [ContentComponent],
+})
+export class ContentModule {}
