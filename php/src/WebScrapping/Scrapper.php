d45f718a7239112e5af9610dbb2e0919e5b30532
André Lucas <andre.lucas.mendes01@aluno.ifce.edu.br>
2024-04-12 10:47:36 -0300

    feature: Obtém os autores de cada paper e seus institutos

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -11,6 +11,13 @@ use Chuva\Php\WebScrapping\Entity\Person;
 class Scrapper {
 
   /**
+   * Normalizes bad-formatted names and return a
+   */
+  private function normalizeName(string $name){
+
+  }
+
+  /**
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
@@ -23,6 +30,21 @@ class Scrapper {
         
         $paper_title = $base_node->getElementsByTagName("h4")[0]->textContent;
         
+        $authors = $base_node->getElementsByTagName("div")[0]->getElementsByTagName("span");
+
+        $paper_authors = [];
+        foreach ($authors as $author) {
+          $author_institutions = $author->getAttribute("title");
+
+          if ($author_institutions != '') {
+              $formatted_name = preg_replace(array('/\s{2,}/'), ' ', $author->textContent);
+              $formatted_name = trim($formatted_name, ' ;');
+              $formatted_name = ucwords(strtolower($formatted_name));
+    
+              $author_name = $formatted_name;
+              $paper_authors[] = new Person($author_name, $author_institutions);
+            }
+        }
       }
     }
 
