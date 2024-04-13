8531f264b6fadf50a34da452e8c2561ed9c1ca55
PedroCezarini <pedro.cezarini@fatec.sp.gov.br>
2024-04-13 14:55:37 -0300

    adicionando os tipos de variaveis da classe paper

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -5,10 +5,14 @@ namespace Chuva\Php\WebScrapping\Entity;
 //The Paper class represents the row of the parsed data.
 
 class Paper {
- public $id; //int
- public $title; //string
- public $type; //string
- public $authors; // \Chuva\Php\WebScrapping\Entity\Person[] essa variavel vem desse vetor.
+ public int $id;
+ public string $title;
+ public string $type;
+ /**
+ * @var \Chuva\Php\WebScrapping\Entity\Person[] //vetor de onde vem $authors
+ */
+ public $authors;
+ 
 
   public function __construct($id, $title, $type, $authors = []) { //construtor pede id, titulo, tipo e autores[];
   }
