b4119b76bfeb5264f8f79eccd2f223c2a5b62e70
Estevan Hernandes Silvade Oliveira <estevanhernandes1@gmail.com>
2024-04-13 15:30:17 -0300

    chore: componente de header e perfil

--- ts/src/app/components/header/header.component.ts
+++ ts/src/app/components/header/header.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  globeIcon: string = 'assets/icons/globe.svg';
+  caretDownIcon: string = 'assets/icons/caret-down.svg';
 
-  constructor() { }
+  personPhoto: string = 'assets/images/person.jfif';
+  email: string = 'alguem12@galoascience.com';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
