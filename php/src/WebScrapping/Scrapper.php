94ca5d67b4dd2932a18f1187d637cbcd84782d77
PedroCezarini <pedro.cezarini@fatec.sp.gov.br>
2024-04-13 15:04:02 -0300

    identando de forma q facilita visualização

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -5,26 +5,21 @@ namespace Chuva\Php\WebScrapping;
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
 
-/**
- * Does the scrapping of a webpage.
- */
 class Scrapper {
-
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
+  
+  //Loads paper information from the HTML and returns the array with the data.
+  //return [new Paper(id,titulo,tipo,[new Pessoa(autorN,instN)])]
   public function scrap(\DOMDocument $dom): array {
     return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
+        new Paper(
+            123,
+            'titulo artigo',
+            'tipo', 
+            [
+                new Person('autor1', 'inst1'),
+                new Person('autor2', 'inst2')
+            ]
+        )
     ];
-  }
-
+}
 }
