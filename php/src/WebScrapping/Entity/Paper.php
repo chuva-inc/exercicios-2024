1fee44cd496cbb460ba92b770c9239b16c1a05c3
jwlds <josewlds@outlook.com>
2024-04-14 16:51:01 -0300

    feat: add web scraping functionality to Scrapper class

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -39,6 +39,9 @@ class Paper {
    * Builder.
    */
   public function __construct($id, $title, $type, $authors = []) {
+    $this->id = $id;
+    $this->title = $title;
+    $this->type = $type;
+    $this->authors = $authors;
   }
-
 }
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -1,30 +1,47 @@
 <?php
 
 namespace Chuva\Php\WebScrapping;
-
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
+use DOMXPath;
 
 /**
  * Does the scrapping of a webpage.
  */
 class Scrapper {
 
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
-  public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
-  }
+    /**
+     * Loads paper information from the HTML and returns the array with the data.
+     */
+    public function scrap(\DOMDocument $dom): array {
+        $xpath = new DOMXPath($dom);
+   
+        $paperNodes = $xpath->query("//a[contains(@class, 'paper-card')]");
+        $papers = [];
+
+        foreach ($paperNodes as $node) {
+            $titleNode = $xpath->query(".//h4[contains(@class, 'paper-title')]", $node);
+            $title = $titleNode->length > 0 ? trim($titleNode->item(0)->textContent) : '';
+
+            $authorNodes = $xpath->query(".//div[contains(@class, 'authors')]/span", $node);
+            $authors = [];
+            foreach ($authorNodes as $authorNode) {
+                $name = trim($authorNode->textContent);
+                $institution = $authorNode->getAttribute("title");
+                $authors[] = new Person($name, $institution);
+            }
+
+            $tagNodes = $xpath->query(".//div[contains(@class, 'tags') and not(contains(@class, 'flex-row'))]", $node);
+            $tags = [];
+            foreach ($tagNodes as $tagNode) {
+                $tags[] = trim($tagNode->textContent);
+            }
+
+            $papers[] = new Paper(0, $title, $tags, $authors);
+        }
+
+        return $papers;
+    }
 
 }
