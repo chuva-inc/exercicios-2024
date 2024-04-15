28e349baab6815f3423f26e3cfd20a73887c1c01
Camila <camila0wolter@gmail.com>
2024-04-15 12:14:37 -0300

    feat: creation of the side menu component

---
+++ ts/src/app/components/aside-menu/aside-menu.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AsideMenuComponent } from './aside-menu.component';
+
+describe('AsideMenuComponent', () => {
+  let component: AsideMenuComponent;
+  let fixture: ComponentFixture<AsideMenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AsideMenuComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AsideMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ts/src/app/components/aside-menu/aside-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aside-menu',
+  templateUrl: './aside-menu.component.html',
+  styleUrls: ['./aside-menu.component.scss']
+})
+export class AsideMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
