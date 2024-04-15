485908f7110ec660696ae66f201c22076526bb07
Pedro Castro <pvmcastro@gmail.com>
2024-04-15 15:22:28 -0300

    Find all the titles

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,7 +14,24 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-  
+    
+    // Find all the titles:
+    $titles = $dom->getElementsByTagName('h4');
+
+    $titles_array = array();
+
+    foreach($titles as $title){
+        $title_text = $title->textContent;
+        $titles_array[] = $title_text;
+    }
+
+    $titles_array = array_slice($titles_array, 4); //
+    array_pop($titles_array);
+
+    echo 'Titulos: <br><br>';
+    print_r($titles_array);
+    echo '<br><br><br><br>';
+
   }
 
 }
