b5205a748d1fc78f2a88ca66d7fcb5786566c7b3
Camila <camila0wolter@gmail.com>
2024-04-15 13:16:52 -0300

    feat: header component creation

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AsideMenuComponent } from './components/aside-menu/aside-menu.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AsideMenuComponent
+    AsideMenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
