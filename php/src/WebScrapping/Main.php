f363e01108082fdc589fff4d00571906cf961b5b
Gabriela Stringasci <145482778+gabrielastringasci@users.noreply.github.com>
2024-04-14 23:08:39 -0300

    Update Main.php
    Atualiza o método run para gerar e salvar uma planilha Excel com os dados extraídos do HTML
    
    Este commit atualiza o método run na classe Main.
    
    Aqui é utilizado a classe Scrapper para extrair dinamicamente os dados do HTML fornecido. Depois, preenche uma planilha Excel (arquivo XLSX) com os dados extraídos. Por fim, salva a planilha Excel com o nome "model.xlsx" no diretório "webscraping" dentro dos ativos do projeto.
    
    Essa atualização melhora significativamente a funcionalidade do método run, proporcionando uma saída mais organizada e estruturada dos resultados do web scraping. Agora, os dados extraídos podem ser facilmente acessados e utilizados para análise posterior, proporcionando uma maior utilidade e valor ao processo de web scraping.

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Chuva\Php\WebScrapping;
-
+require_once 'Scrapper.php'; // Importe a classe Scrapper
+require_once 'PHPExcel.php'; // Importe a biblioteca PHPExcel
 /**
  * Runner for the Webscrapping exercice.
  */
@@ -10,14 +11,30 @@ class Main {
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
-  public static function run(): void {
+   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
     $data = (new Scrapper())->scrap($dom);
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
-  }
+    // Escrever os dados em um arquivo XLSX
+    $objPHPExcel = new \PHPExcel();
+    $objPHPExcel->setActiveSheetIndex(0);
+    $sheet = $objPHPExcel->getActiveSheet();
 
+    // Preencher o arquivo XLSX com os dados extraídos
+    $row = 1;
+    foreach ($data as $paper) {
+        $sheet->setCellValue('A' . $row, $paper->title);
+        $sheet->setCellValue('B' . $row, $paper->type);
+        // Adicione mais células e dados conforme necessário
+        $row++;
+    }
+
+    // Salvar o arquivo
+    $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+    $objWriter->save(__DIR__ . '/../../webscraping/model.xlsx');
+  }
 }
+
+Main::run();
