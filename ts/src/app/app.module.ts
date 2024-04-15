57ee443731cb5f7de6e5a420f748720238a4353b
ndbzika <chakcpps@gmail.com>
2024-04-15 19:20:43 -0300

    feat: footer component

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, HeaderComponent],
+  declarations: [AppComponent, MenuComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
