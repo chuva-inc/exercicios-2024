c08ff613bcbb4aa34b686606f6da93f81c8ce86b
Camila <camila0wolter@gmail.com>
2024-04-15 17:24:31 -0300

    feat: footer component creation

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AsideMenuComponent } from './components/aside-menu/aside-menu.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AsideMenuComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
