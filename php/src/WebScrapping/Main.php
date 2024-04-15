0d9dfcee4ed7dcb0ed1c05408acc46ac345ef075
Pedro Castro <pvmcastro@gmail.com>
2024-04-15 18:00:59 -0300

    @ before loadHTMLFile to suppress error warnings

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -12,7 +12,7 @@ class Main {
    */
   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+    @$dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
     $data = (new Scrapper())->scrap($dom);
 
