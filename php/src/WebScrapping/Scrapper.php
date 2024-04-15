17653b8043015e12dd7f503024d3f290c2351973
PedroCezarini <pedro.cezarini@fatec.sp.gov.br>
2024-04-14 22:57:35 -0300

    adicionando comentarios pra ficar mais claro como ta a construçao da funçao scrap() e adicionei assinatura da funçao parsePaperFromLink() q ta senod chamada na funçao scrap

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -13,13 +13,17 @@ class Scrapper {
     $papers = [];
 
         $links = $dom->getElementsByTagName('a'); //tentando fazer de forma q sempre que ache a tag <a href=...> identifique q é um novo paper
-        foreach ($links as $link) {
-            $paper = $this->parsePaperFromLink($link);
+        foreach ($links as $link) { //cada tag (q é novo paper) ficara salva como elemnto no vetor links.. 
+            $paper = $this->parsePaperFromLink($link); //aqui estou chamando uma outra funçao que vai preencher o construtor do paper com as informações q tão dentro de cada elemento link do vetor links...
             if ($paper) {
-                $papers[] = $paper;
+                $papers[] = $paper; //se paper existir (oq sabemos q sim) ele preenche o elemento do vetor com os dados do $paper instanciado
             }
         }
 
         return $papers;
+    }
+
+    private function parsePaperFromLink(\DOMElement $link): ?Paper {
+
     }
 }
