3b03f0d842eccedec751a49e06d82830dd9b87a7
Alexandre Ribeiro <alexandreribeiro0209@gmail.com>
2024-04-17 18:14:59 -0300

    Update Main.php

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,6 +2,9 @@
 
 namespace Chuva\Php\WebScrapping;
 
+require_once '../vendor/box/spout/src/Spout/Autoloader/autoload.php';
+
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 /**
  * Runner for the Webscrapping exercice.
  */
@@ -10,14 +13,89 @@ class Main {
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
-  public static function run(): void {
-    $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+  public static function gerarPlanilha(): void {
 
-    $data = (new Scrapper())->scrap($dom);
+  $file = 'planilhaProceedings.xlsx';
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
+  $writer = WriterEntityFactory::createXLSXWriter();
+  $writer->openToBrowser($file);
+
+  // Escrevendo cabecalho
+  $cabecalho = ['ID', 'Title', 'Type', 'Author 1', 'Author 1 Institution', 'Author 2', 'Author 2 Institution', 'Author 3', 'Author 3 Institution', 'Author 4', 'Author 4 Institution', 'Author 5', 'Author 5 Institution', 'Author 6', 'Author 6 Institution', 'Author 7', 'Author 7 Institution', 'Author 8', 'Author 8 Institution', 'Author 9', 'Author 9 Institution','Author 10', 'Author 10 Institution'];
+
+  // Adiciona uma nova linha com os valores
+  $rowFromValues = WriterEntityFactory::createRowFromArray($cabecalho);
+  $writer->addRow($rowFromValues); 
+
+  // Utilizando DOMDocument para acessar o conteudo da pagina
+  $html = file_get_contents(__DIR__ . '/../../assets/origin.html');
+  $dom = new \DOMDocument;
+
+  // Carregando o HTML da pagina
+  @$dom->loadHTML($html);
+
+  // Criacao de um objeto DOMXPath
+  $xpath = new \DOMXPath($dom);
+
+  // Pega as informacoes de tudo dentros dos elementos ancora da pagina
+  $links = $xpath->query("//a[contains(@class, 'paper-card')]");
+
+  // Declaracao de variaveis
+  $count = 0;
+
+  // Para cada elemento link dentro do elemento links
+  foreach ($links as $link){
+
+      // Transforma a variável autores em array
+      $informacoes = array();
+
+      // Declarao de variaveis
+      $i=0;
+
+      // Selecionar a div com classe "volume-info"
+      $ID = $xpath->query("//div[contains(@class, 'volume-info')]")->item($count);
+      $ID = $ID->nodeValue;
+
+      // Selecionar a h4 com classe "paper-title"
+      $titulo = $xpath->query("//h4[contains(@class, 'paper-title')]")->item($count);
+      $titulo = $titulo->nodeValue;
+
+      // Selecionar a div com classe "tags mr-sm"
+      $type = $xpath->query("//div[contains(@class, 'tags mr-sm')]")->item($count);
+      $type = $type->nodeValue;
+
+      // Pega as informacoes de todos os elementos span dentro do elemento ancora
+      $nomes = $link->getElementsByTagName('span');
+
+      $autores = array();
+
+      // Para nome de autor executa
+      foreach ($nomes as $nome) {
+
+          $instituicao = $nome->getAttribute('title');
+          $author = $nome->nodeValue;
+
+          // Adiciona o nome do autor e sua instituição ao array de autores
+          $autores[] = $author;
+          $autores[] = $instituicao;
+
+          $i++;
+      }
+
+      // Adicionando informações do artigo e autores em uma única linha
+      $informacoes = array_merge(array($ID,$titulo,$type), $autores);
+
+      // Criando uma única linha para adicionar ao escritor
+      $insert = WriterEntityFactory::createRowFromArray($informacoes);
+      $writer->addRow($insert);
+      $count++;
+  }
+    
+  $writer->close();
+
   }
 
 }
+
+$gerar = new Main();
+$gerar->gerarPlanilha();
---
+++ php/src/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'estudophp/php-basic',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'box/spout' => array(
+            'pretty_version' => 'v3.3.0',
+            'version' => '3.3.0.0',
+            'reference' => '9bdb027d312b732515b884a341c0ad70372c6295',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../box/spout',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'estudophp/php-basic' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
