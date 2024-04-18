01613676458a63c8f7739a1f615bf7d89f60cf13
Ranie <143940800+ranyesantos@users.noreply.github.com>
2024-04-18 16:45:52 -0300

    implementação de queries xpath

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,17 +14,14 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
-  }
+    
+    // Crie um objeto DOMXPath para consulta
+    $xpath = new \DOMXPath($dom);
 
+    // Consulta XPath para obter os elementos que contêm atributos de papers e de person
+    $idsQuery = $xpath->query('//div[@class="volume-info"]');
+    $typesQuery = $xpath->query('//div[@class="tags mr-sm"]');
+    $authorsQuery = $xpath->query('//div[@class="authors"]//span');
+    $titlesQuery = $xpath->query('//h4[@class="my-xs paper-title"]');
+    
 }
