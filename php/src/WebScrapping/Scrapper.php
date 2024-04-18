34c5179bcdc25cb3cd3b1339bc72f3f986d113da
PedroCezarini <pedro.cezarini@fatec.sp.gov.br>
2024-04-18 11:40:00 -0300

    fazendo com xpath agora, porque do outro jeito usando só o dom eu não tava conseguindo XD

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -10,20 +10,14 @@ class Scrapper {
   //Loads paper information from the HTML and returns the array with the data.
   //return [new Paper(id,titulo,tipo,[new Pessoa(autorN,instN)])]
   public function scrap(\DOMDocument $dom): array {
-    $papers = [];
 
-        $links = $dom->getElementsByTagName('a'); //tentando fazer de forma q sempre que ache a tag <a href=...> identifique q é um novo paper
-        foreach ($links as $link) { //cada tag (q é novo paper) ficara salva como elemnto no vetor links.. 
-            $paper = $this->parsePaperFromLink($link); //aqui estou chamando uma outra funçao que vai preencher o construtor do paper com as informações q tão dentro de cada elemento link do vetor links...
-            if ($paper) {
-                $papers[] = $paper; //se paper existir (oq sabemos q sim) ele preenche o elemento do vetor com os dados do $paper instanciado
-            }
-        }
+    $xpath = new \DOMXPath($dom); //inicializa uma variavel xpath
+// Definir os caminhos XPath para os elementos que queremos extrair
+    $tituloXPath = "//h4";
+    $instituicaoXPath = "//span[@title='institution']";
+    $autorXPath = "//span[@title='person']";
+    $tipoXPath = "//div[@class='tags mr-sn']";
+    $idXPath = "//div[@class='volume-info']";
 
-        return $papers;
-    }
-
-    private function parsePaperFromLink(\DOMElement $link): ?Paper {
-
-    }
+}
 }
