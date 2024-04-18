abc47d6bfda6471234c8108a75a9ad4b80b9fab2
Pedro Castro <pvmcastro@gmail.com>
2024-04-18 15:27:56 -0300

    setColumnWidth

--- php/src/WebScrapping/Excel.php
+++ php/src/WebScrapping/Excel.php
@@ -33,6 +33,14 @@ class Excel {
         $writer = new Writer();
         $writer->openToFile($filepath);
 
+
+        // Set the columns width:
+        $options->setColumnWidth(15.0, 2);
+        $options->setColumnWidth(5.0, 3);
+        for($i = 4; $i < $this->maxAuthors($papers)*2; $i++){
+            $options->setColumnWidth(7.0, $i);
+        }
+
         // Creates an array with the headers values
         $headers = ['ID', 'Title', 'Type'];
         $maxAuthor = $this->maxAuthors($papers);
