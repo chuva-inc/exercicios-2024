c4fb8d0cf65ca04c185fb5b77b67a7647e41718c
PedroCezarini <pedro.cezarini@fatec.sp.gov.br>
2024-04-18 12:29:54 -0300

    corrigido o caminho xpath para conseguir ler o autor

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -15,34 +15,32 @@ class Scrapper {
     // definindo os caminhos XPath para os elementos que queremos extrair
     $tituloXPath = "//h4[@class='my-xs paper-title']";
     $instituicaoXPath = "//span[@title='institution']";
-    $autorXPath = "//span[@title='person']";
+    $autorXPath = "//div[@class='authors']/span";
     $tipoXPath = "//div[@class='tags mr-sn']";
     $idXPath = "//div[@class='volume-info']";
-
-    //queries do xpath
+    
+    // Extrair os dados usando XPath
     $titulos = $xpath->query($tituloXPath);
     $instituicoes = $xpath->query($instituicaoXPath);
     $autores = $xpath->query($autorXPath);
     $tipos = $xpath->query($tipoXPath);
     $ids = $xpath->query($idXPath);
-
-    //for que vai iterar sobre cada paper e identificar os atributos q queremos
-    for ($i = 0; $i < $titulos->length; $i++) { 
+    
+    // Iterar sobre os resultados e exibir os dados
+    for ($i = 0; $i < $titulos->length; $i++) {
         $titulo = $titulos->item($i)->nodeValue;
         $instituicao = $instituicoes->item($i)->nodeValue;
         $autor = $autores->item($i)->nodeValue;
         $tipo = $tipos->item($i)->nodeValue;
         $id = $ids->item($i)->nodeValue;
-
-        //imprimindo os dados q estao sendo lidos
-      echo "Título: $titulo<br>";
-      echo "Instituição: $instituicao<br>";
-      echo "Autor: $autor<br>";
-      echo "Tipo: $tipo<br>";
-      echo "ID: $id<br>";
-      echo "<br>";
-
+    
+        echo "Título: $titulo<br>";
+        echo "Instituição: $instituicao<br>";
+        echo "Autor: $autor<br>";
+        echo "Tipo: $tipo<br>";
+        echo "ID: $id<br>";
+        echo "<br>";
     }
-}
-}
-  
+    }
+    }
+    
