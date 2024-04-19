4d96a9480b3446d9e17d4c4c57c3af5562421ed5
Ranie <143940800+ranyesantos@users.noreply.github.com>
2024-04-19 10:48:52 -0300

    adição de comentários sobre o algoritmo para gerar o cabeçalho

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -17,30 +17,43 @@ class Main {
     $allData = (new Scrapper())->scrap($dom);
 
     // Write your logic to save the output file bellow.
+    
+    // Define o arquivo de saída
     $outputFilePath = "result.xlsx";
 
+    // Cria um novo objeto Writer para criar um arquivo XLSX
     $writer = WriterEntityFactory::createXLSXWriter();
+    // Abre o arquivo de saída para escrita
     $writer->openToFile($outputFilePath);
 
+    // Define as informações fixas do cabeçalho da planilha
     $header = [ "ID","Title","Type"];
-    
+
+    // Inicializa a variável para armazenar o número máximo de autores
     $authorsTotal = 0;
 
+    // Itera sobre todos os dados para determinar o número máximo de autores em um único item de dados
     foreach ($allData as $singleData) {
       $authorsCount = $singleData->getAuthorCount();
+      // Verifica se o número de autores em $authorsCount é maior que o número máximo atual de autores
       if ($authorsCount > $authorsTotal) {
+        // Atualiza o número total de autores se a condicional for verdadeira
         $authorsTotal = $authorsCount;
       }
     }
 
+    // Adiciona autor e instituição ao cabeçalho da planilha
     for ($i = 1; $i <= $authorsTotal; $i++) {
-      $header[] = "Author $i";
-      $header[] = "Author $i Institution";
+      $header[] = "Author $i";               // Adiciona coluna para o nome do autor
+      $header[] = "Author $i Institution";   // Adiciona coluna para a instituição do autor
     }
 
+    // Cria uma linha de cabeçalho da planilha usando os dados do cabeçalho
     $headerRow = WriterEntityFactory::createRowFromArray($header);
+    // Adiciona a linha de cabeçalho à planilha
     $writer->addRow($headerRow);
 
+    // Fecha o arquivo de saída
     $writer->close();
 
   }
