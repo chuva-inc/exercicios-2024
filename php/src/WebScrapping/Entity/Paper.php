bc5a8f62bcf1acea1c068053a72a244beccb950b
PedroCezarini <pedro.cezarini@fatec.sp.gov.br>
2024-04-24 22:12:31 -0300

    ainda tentando entender se erro de nao passar nos testes eh identaçao

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -6,29 +6,47 @@ namespace Chuva\Php\WebScrapping\Entity;
  * The Paper class represents the row of the parsed data.
  */
 class Paper {
-    public int $id;
-    public string $title;
-    public string $type;
+  /**
+   * Paper ID.
+   *
+   * @var int
+   */
+  public int $id;
 
-    /**
-     * @var \Chuva\Php\WebScrapping\Entity\Person[] Vetor de onde vem authors
-     */
-    public $authors;
+  /**
+   * Paper title.
+   *
+   * @var string
+   */
+  public string $title;
 
-    /**
-     * Paper constructor.
-     *
-     * @param int     $id      Paper ID
-     * @param string  $title   Paper title
-     * @param string  $type    Paper type
-     * @param array   $authors Array of authors
-     */
-    public function __construct(int $id, string $title, string $type, array $authors = []) {
-      // Constructor pede id, title, type, e authors
-      $this->id = $id;
-      $this->title = $title;
-      $this->type = $type;
-      $this->authors = $authors;
+  /**
+   * Paper type.
+   *
+   * @var string
+   */
+  public string $type;
+
+  /**
+   * Array of authors.
+   *
+   * @var \Chuva\Php\WebScrapping\Entity\Person[]
+   */
+  public $authors;
+
+  /**
+   * Paper constructor.
+   *
+   * @param int     $id      Paper ID
+   * @param string  $title   Paper title
+   * @param string  $type    Paper type
+   * @param array   $authors Array of authors
+   */
+  public function __construct(int $id, string $title, string $type, array $authors = []) {
+    // Constructor requires id, title, type, and authors
+    $this->id = $id;
+    $this->title = $title;
+    $this->type = $type;
+    $this->authors = $authors;
   }
 }
- 
