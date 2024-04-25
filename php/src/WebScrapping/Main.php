994d9cce9f519463310f05935518537829329052
PedroCezarini <pedro.cezarini@fatec.sp.gov.br>
2024-04-24 22:56:43 -0300

    corrigindo parte dos erros do lint na main

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -9,69 +9,67 @@ use OpenSpout\Writer\XLSX\Writer;
 /**
  * Runner for the Web scraping exercise.
  */
-class Main
-{
+class Main {
 
     /**
-     * Main runner, instantiates a Scrapper and runs.
+  * Main runner, instantiates a Scrapper and runs.
      */
-    public static function run(): void
-    {
-        $dom = new \DOMDocument('1.0', 'utf-8');
-        @$dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+    public static function run(): void {
+    $dom = new \DOMDocument('1.0', 'utf-8');
+    @$dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-        $papers = (new Scrapper())->scrap($dom);
+    $papers = (new Scrapper())->scrap($dom);
 
-        $writer = new Writer();
-        $writer->openToFile(__DIR__ . '/../../assets/scrappedData.xlsx');
+    $writer = new Writer();
+    $writer->openToFile(__DIR__ . '/../../assets/scrappedData.xlsx');
 
-        // Adding spreadsheet header
-        $header = [
-            Cell::fromValue('ID'),
-            Cell::fromValue('Title'),
-            Cell::fromValue('Type'),
-            Cell::fromValue('Author 1'),
-            Cell::fromValue('Author 1 Institution'),
-            Cell::fromValue('Author 2'),
-            Cell::fromValue('Author 2 Institution'),
-            Cell::fromValue('Author 3'),
-            Cell::fromValue('Author 3 Institution'),
-            Cell::fromValue('Author 4'),
-            Cell::fromValue('Author 4 Institution'),
-            Cell::fromValue('Author 5'),
-            Cell::fromValue('Author 5 Institution'),
-            Cell::fromValue('Author 6'),
-            Cell::fromValue('Author 6 Institution'),
-            Cell::fromValue('Author 7'),
-            Cell::fromValue('Author 7 Institution'),
-            Cell::fromValue('Author 8'),
-            Cell::fromValue('Author 8 Institution'),
-            Cell::fromValue('Author 9'),
-            Cell::fromValue('Author 9 Institution'),
-            Cell::fromValue('Author 10'),
-            Cell::fromValue('Author 10 Institution'),
-            Cell::fromValue('Author 11'),
-            Cell::fromValue('Author 11 Institution'),
-            Cell::fromValue('Author 12'),
-            Cell::fromValue('Author 12 Institution'),
-            Cell::fromValue('Author 13'),
-            Cell::fromValue('Author 13 Institution'),
-            Cell::fromValue('Author 14'),
-            Cell::fromValue('Author 14 Institution'),
-            Cell::fromValue('Author 15'),
-            Cell::fromValue('Author 15 Institution'),
-            Cell::fromValue('Author 16'),
-            Cell::fromValue('Author 16 Institution'),
-            Cell::fromValue('Author 17'),
-            Cell::fromValue('Author 17 Institution'),
-            Cell::fromValue('Author 18'),
-            Cell::fromValue('Author 18 Institution'),
-            Cell::fromValue('Author 19'),
-            Cell::fromValue('Author 19 Institution'),
+        // Adding spreadsheet header.
+    $header = [
+          Cell::fromValue('ID'),
+          Cell::fromValue('Title'),
+          Cell::fromValue('Type'),
+          Cell::fromValue('Author 1'),
+          Cell::fromValue('Author 1 Institution'),
+          Cell::fromValue('Author 2'),
+          Cell::fromValue('Author 2 Institution'),
+          Cell::fromValue('Author 3'),
+          Cell::fromValue('Author 3 Institution'),
+          Cell::fromValue('Author 4'),
+          Cell::fromValue('Author 4 Institution'),
+          Cell::fromValue('Author 5'),
+          Cell::fromValue('Author 5 Institution'),
+          Cell::fromValue('Author 6'),
+          Cell::fromValue('Author 6 Institution'),
+          Cell::fromValue('Author 7'),
+          Cell::fromValue('Author 7 Institution'),
+          Cell::fromValue('Author 8'),
+          Cell::fromValue('Author 8 Institution'),
+          Cell::fromValue('Author 9'),
+          Cell::fromValue('Author 9 Institution'),
+          Cell::fromValue('Author 10'),
+          Cell::fromValue('Author 10 Institution'),
+          Cell::fromValue('Author 11'),
+          Cell::fromValue('Author 11 Institution'),
+          Cell::fromValue('Author 12'),
+          Cell::fromValue('Author 12 Institution'),
+          Cell::fromValue('Author 13'),
+          Cell::fromValue('Author 13 Institution'),
+          Cell::fromValue('Author 14'),
+          Cell::fromValue('Author 14 Institution'),
+          Cell::fromValue('Author 15'),
+          Cell::fromValue('Author 15 Institution'),
+          Cell::fromValue('Author 16'),
+          Cell::fromValue('Author 16 Institution'),
+          Cell::fromValue('Author 17'),
+          Cell::fromValue('Author 17 Institution'),
+          Cell::fromValue('Author 18'),
+          Cell::fromValue('Author 18 Institution'),
+          Cell::fromValue('Author 19'),
+          Cell::fromValue('Author 19 Institution'),
         ];
 
-        $singleRow = new Row($header);
-        $writer->addRow($singleRow);
+    $singleRow = new Row($header);
+    $writer->addRow($singleRow);
 
         foreach ($papers as $paper) {
             // Create a row for each paper
