8d4179b3f3b8d904854ab504e9de9a636ed84ac8
PedroCezarini <pedro.cezarini@fatec.sp.gov.br>
2024-04-24 22:20:44 -0300

    testando o codesniffer, conforme orientado nos erros do PHPlint

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -5,48 +5,50 @@ namespace Chuva\Php\WebScrapping\Entity;
 /**
  * The Paper class represents the row of the parsed data.
  */
-class Paper {
-  /**
-   * Paper ID.
-   *
-   * @var int
-   */
-  public int $id;
+class Paper
+{
+    /**
+     * Paper ID.
+     *
+     * @var int
+     */
+    public int $id;
 
-  /**
-   * Paper title.
-   *
-   * @var string
-   */
-  public string $title;
+    /**
+     * Paper title.
+     *
+     * @var string
+     */
+    public string $title;
 
-  /**
-   * Paper type.
-   *
-   * @var string
-   */
-  public string $type;
+    /**
+     * Paper type.
+     *
+     * @var string
+     */
+    public string $type;
 
-  /**
-   * Array of authors.
-   *
-   * @var \Chuva\Php\WebScrapping\Entity\Person[]
-   */
-  public $authors;
+    /**
+     * Array of authors.
+     *
+     * @var \Chuva\Php\WebScrapping\Entity\Person[]
+     */
+    public $authors;
 
-  /**
-   * Paper constructor.
-   *
-   * @param int     $id      Paper ID
-   * @param string  $title   Paper title
-   * @param string  $type    Paper type
-   * @param array   $authors Array of authors
-   */
-  public function __construct(int $id, string $title, string $type, array $authors = []) {
-    // Constructor requires id, title, type, and authors
-    $this->id = $id;
-    $this->title = $title;
-    $this->type = $type;
-    $this->authors = $authors;
-  }
+    /**
+     * Paper constructor.
+     *
+     * @param int    $id      Paper ID
+     * @param string $title   Paper title
+     * @param string $type    Paper type
+     * @param array  $authors Array of authors
+     */
+    public function __construct(int $id, string $title, string $type, array $authors = [])
+    {
+        // Constructor requires id, title, type, and authors
+        $this->id = $id;
+        $this->title = $title;
+        $this->type = $type;
+        $this->authors = $authors;
+    }
 }
--- php/src/WebScrapping/Entity/Person.php
+++ php/src/WebScrapping/Entity/Person.php
@@ -2,15 +2,17 @@
 
 namespace Chuva\Php\WebScrapping\Entity;
 
-class Person {
+class Person
+{
 
-public string $name;
-public string $institution;
+    public string $name;
+    public string $institution;
 
-//construtor recebe nome e instituicao do autor
-public function __construct($name, $institution) {
-    $this->name = $name;
-    $this->institution = $institution;
-  }
+    //construtor recebe nome e instituicao do autor
+    public function __construct($name, $institution)
+    {
+        $this->name = $name;
+        $this->institution = $institution;
+    }
 
 }
