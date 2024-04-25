37ce77278131019b8c123e4879bd12b1d92d2e4b
PedroCezarini <pedro.cezarini@fatec.sp.gov.br>
2024-04-24 22:31:22 -0300

    tentando

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -38,13 +38,13 @@ class Paper {
    * Paper constructor.
    *
    * @param int $id
-   * Paper ID must be a positive integer.
+   *    Paper ID must be a positive integer.
    * @param string $title   
-   * Paper title is the title of the paper.
+   *    Paper title is the title of the paper.
    * @param string $type
-   * Paper type the type of the paper.
+   *    Paper type the type of the paper.
    * @param array  $authors 
-   * Array of authors the authors of the paper.
+   *    Array of authors the authors of the paper.
    */
   public function __construct(int $id, string $title, string $type, array $authors = [])
   {
