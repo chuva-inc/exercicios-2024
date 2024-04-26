ef0e6e122bcdd0c65fc5289186d6e28467bab39a
Pedro Castro <pvmcastro@gmail.com>
2024-04-26 15:42:07 -0300

    fixes indentation and spaces

--- php/src/WebScrapping/Excel.php
+++ php/src/WebScrapping/Excel.php
@@ -16,7 +16,8 @@ class Excel {
   /**
    * Defines the maximum number of authors of a paper from an array of papers.
    */
-  public function maxAuthors(Array $papers): int {
+  
+   public function maxAuthors(Array $papers): int {
     $max = 0;
 
     foreach ($papers as $paper) {
@@ -31,49 +32,49 @@ class Excel {
    */
   public function export(Array $papers): void {
 
-    // Set the columns width
+    // Set the columns width.
     $options = new Options();
     $options->setColumnWidth(45.0, 2);
     $options->setColumnWidth(15.0, 3);
     $maxAuthor = $this->maxAuthors($papers);
-    for($i = 4; $i < $maxAuthor*2; $i++){
+    for ($i = 4; $i < $maxAuthor * 2; $i++) {
       $options->setColumnWidth(21.0, $i);
     }
 
-    // Creates and open a xlsx file
+    // Creates and open a xlsx file.
     $writer = new Writer($options);
     $filepath = __DIR__ . '/../../assets/data.xlsx';
     $writer->openToFile($filepath);
 
-    // Creates an array with the headers values
+    // Creates an array with the headers values.
     $headers = ['ID', 'Title', 'Type'];
     for ($i = 0; $i < $maxAuthor; $i++) {
       $headers[] = "Author " . ($i + 1);
       $headers[] = "Author " . ($i + 1) . " Institution";
     }
 
-    // Creates a sytle for the header
+    // Creates a sytle for the header.
     $headerStyle = (new Style())
-    ->setFontBold()
-    ->setFontSize(11)
-    ->setFontName('Arial')
-    ->setShouldWrapText(false);
+      ->setFontBold()
+      ->setFontSize(11)
+      ->setFontName('Arial')
+      ->setShouldWrapText(false);
 
     // Creates the headers row
     $headers = Row::fromValues($headers, $headerStyle);
 
-    // Adds the row in the spreadsheet
+    // Adds the row in the spreadsheet.
     $writer->addRow($headers);
 
-    // Creates a style for the content
+    // Creates a style for the content.
     $contentStyle = (new Style())
-    ->setFontSize(11)
-    ->setFontName('Arial')
-    ->setShouldWrapText(false);
+      ->setFontSize(11)
+      ->setFontName('Arial')
+      ->setShouldWrapText(false);
 
-    foreach ($papers as $paper){
+    foreach ($papers as $paper) {
       $row = [$paper->id, $paper->title, $paper->type];
-      foreach ($paper->authors as $author){
+      foreach ($paper->authors as $author) {
         $row[] = $author->name;
         $row[] = $author->institution;
       }
@@ -81,10 +82,11 @@ class Excel {
       $writer->addRow($row);
     }
 
-    // Closes the writer and saves the file
+    // Closes the writer and saves the file.
     $writer->close();
 
   }
+  
 }
 
 
