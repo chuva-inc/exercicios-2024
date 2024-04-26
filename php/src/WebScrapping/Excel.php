724f5140e122622cef44d7ad1e8ea968edde54d0
Pedro Castro <pvmcastro@gmail.com>
2024-04-26 08:52:46 -0300

    fixes space indentation and creates method docs comments

--- php/src/WebScrapping/Excel.php
+++ php/src/WebScrapping/Excel.php
@@ -4,84 +4,88 @@ namespace Chuva\Php\WebScrapping;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-use OpenSpout\Writer\XLSX\Writer;
-use OpenSpout\Writer\XLSX\Options;
-use OpenSpout\Common\Entity\Style\Style;
 use OpenSpout\Common\Entity\Row;
+use OpenSpout\Common\Entity\Style\Style;
+use OpenSpout\Writer\XLSX\Options;
+use OpenSpout\Writer\XLSX\Writer;
 
+/**
+ * Exports data to a spreadsheet.
+ */
 class Excel {
+  
+  /**
+   * Defines the maximum number of authors of a paper from an array of papers.
+   */
+  public function maxAuthors(Array $papers): int {
+    $max = 0;
 
-    
-    public function maxAuthors(Array $papers): int {
-        $max = 0;
-        
-        foreach($papers as $paper){
-            $max = max($max, $paper->countAuthors());
-        }
-        
-        return $max;
+    foreach($papers as $paper){
+    $max = max($max, $paper->countAuthors());
     }
 
-    /**
-    * Exports data information to a spreadsheet.
-    */
-    public function export(Array $papers): void{
+    return $max;
+  }
 
-        // Creates and open a xlsx file
-        $filepath = __DIR__ . '/../../assets/data.xlsx';
-        $options = new Options();
-        $writer = new Writer($options);
-        $writer->openToFile($filepath);
-        
+  /**
+   * Exports all data from an array of papers to a spreadsheet.
+   */
+  public function export(Array $papers): void{
 
-        // Set the columns width
-        $options->setColumnWidth(15.0, 2);
-        $options->setColumnWidth(5.0, 3);
-        $maxAuthor = $this->maxAuthors($papers);
-        for($i = 4; $i < $maxAuthor*2; $i++){
-            $options->setColumnWidth(7.0, $i);
-        }
-
-        // Creates an array with the headers values
-        $headers = ['ID', 'Title', 'Type'];
-        for ($i = 0; $i < $maxAuthor; $i++) {
-            $headers[] = "Author " . ($i + 1);
-            $headers[] = "Author " . ($i + 1) . " Institution";
-        }
-
-        // Creates a sytle for the header
-        $headerStyle = (new Style())
-            ->setFontBold()
-            ->setFontSize(11)
-            ->setFontName('Arial')
-            ->setShouldWrapText(false);
-
-        // Creates the headers row
-        $headers = Row::fromValues($headers, $headerStyle);
-
-        // Adds the row in the spreadsheet
-        $writer->addRow($headers);
-        
-        // Creates a style for the content
-        $contentStyle = (new Style())
-            ->setFontSize(11)
-            ->setFontName('Arial')
-            ->setShouldWrapText(false);
-        
-        foreach ($papers as $paper){
-            $row = [$paper->id, $paper->title, $paper->type];
-            foreach ($paper->authors as $author){
-                $row[] = $author->name;
-                $row[] = $author->institution;
-            }
-            $row = Row::fromValues($row, $contentStyle);
-            $writer->addRow($row);
-        }
-
-        // Closes the writer and saves the file
-        $writer->close();
-       
+    // Set the columns width
+    $options = new Options();
+    $options->setColumnWidth(45.0, 2);
+    $options->setColumnWidth(15.0, 3);
+    $maxAuthor = $this->maxAuthors($papers);
+    for($i = 4; $i < $maxAuthor*2; $i++){
+      $options->setColumnWidth(21.0, $i);
     }
+
+    // Creates and open a xlsx file
+    $writer = new Writer($options);
+    $filepath = __DIR__ . '/../../assets/data.xlsx';
+    $writer->openToFile($filepath);
+
+    // Creates an array with the headers values
+    $headers = ['ID', 'Title', 'Type'];
+    for ($i = 0; $i < $maxAuthor; $i++) {
+      $headers[] = "Author " . ($i + 1);
+      $headers[] = "Author " . ($i + 1) . " Institution";
+    }
+
+    // Creates a sytle for the header
+    $headerStyle = (new Style())
+    ->setFontBold()
+    ->setFontSize(11)
+    ->setFontName('Arial')
+    ->setShouldWrapText(false);
+
+    // Creates the headers row
+    $headers = Row::fromValues($headers, $headerStyle);
+
+    // Adds the row in the spreadsheet
+    $writer->addRow($headers);
+
+    // Creates a style for the content
+    $contentStyle = (new Style())
+    ->setFontSize(11)
+    ->setFontName('Arial')
+    ->setShouldWrapText(false);
+
+    foreach ($papers as $paper){
+      $row = [$paper->id, $paper->title, $paper->type];
+      foreach ($paper->authors as $author){
+        $row[] = $author->name;
+        $row[] = $author->institution;
+      }
+      $row = Row::fromValues($row, $contentStyle);
+      $writer->addRow($row);
+    }
+
+    // Closes the writer and saves the file
+    $writer->close();
+
+  }
 }
 
 
