86d5eadaddd587b9d2528ce56d4838a17531bdc3
Jonatas da  Mata <jonatasdamata1@hotmail.com>
2024-05-01 11:51:33 -0300

    feat: Adicionando a lógica no arquivo Scrapper.php

--- src/WebScrapping/Scrapper.php
+++ src/WebScrapping/Scrapper.php
@@ -14,17 +14,53 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
-  }
+    $xpath = new \DOMXPath($dom);
+
+    $data = [];
+
+    // Encontrar todos os elementos <a> com a classe "paper-card"
+    $nodes = $xpath->query('//a[contains(@class, "paper-card")]');
+
+    foreach ($nodes as $node) {
+        // Extrair o ID do link
+        $id = basename($node->getAttribute('href'));
+
+        // Extrair o título
+        $title = $node->getElementsByTagName('h4')->item(0)->nodeValue;
+
+        // Extrair os autores e suas instituições
+        $authors = [];
+        $authorsElements = $node->getElementsByTagName('span');
+        foreach ($authorsElements as $authorElement) {
+            $author = $authorElement->nodeValue;
+            $institution = $authorElement->getAttribute('title');
+            $authors[] = [
+                'name' => $author,
+                'institution' => $institution,
+            ];
+        }
+
+        // Extrair o tipo da apresentação
+        $type = '';
+        $tagsElements = $node->getElementsByTagName('div');
+        foreach ($tagsElements as $tagElement) {
+            if ($tagElement->getAttribute('class') === 'tags mr-sm') {
+                $type = $tagElement->nodeValue;
+                break;
+            }
+        }
+
+        // Adicionar os dados ao array
+        $data[] = [
+            'id' => $id,
+            'title' => $title,
+            'type' => $type,
+            'authors' => $authors,
+        ];
+    }
+
+    return $data;
+}
+
 
 }
