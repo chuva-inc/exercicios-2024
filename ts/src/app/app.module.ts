96254db89fe6746114a37b79395c41c6753ded45
Eduarda-Elisa <eduardaelisadasilva@gmail.com>
2024-05-06 19:28:05 -0300

    create header component and menu component

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './appComponent/app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
