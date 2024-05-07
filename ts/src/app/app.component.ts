8898ad59cbe2fc3d00db66ca46a4ebd247e948a4
lucas.lugao <lucasbrumfurtado@hotmail.com>
2024-05-07 16:59:44 -0300

    feature: implementando função de ver menos resumo / melhorando função para separar de acordo com os a tag de quebrar linha

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -104,14 +104,15 @@ export class AppComponent {
       number: 4,
     },
   ];
-  textoResumo =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis feis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut.  Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum.  Etiam aliquam dictum nisl, vel aliquet enim accumsan sit ametl accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsass ' +
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis feis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut.  Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum.  Etiam aliquam dictum nisl, vel aliquet enim accumsan sit ametl accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsass ' +
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis feis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut.  Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum.  Etiam aliquam dictum nisl, vel aliquet enim accumsan sit ametl accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsant accumsass ';
-
+  textoResumoArray: string[] = [];
+  textoResumo: string =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis felis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut. Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum. \n' +
+    'Fusce vitae luctus dui. Donec id euismod mauris, in volutpat urna. Proin dapibus consequat feugiat. Proin dictum justo arcu, quis vestibulum augue lacinia quis. Sed dignissim dui nulla, ut faucibus mauris sodales id. Aliquam erat volutpat. Maecenas dolor enim, tincidunt id elit non, suscipit interdum turpis. Etiam finibus urna libero, eget interdum eros volutpat ullamcorper. Pellentesque et pretium lorem. Aenean interdum quis diam ac porttitor. Cras nec ipsum pulvinar, pharetra libero non, ornare enim. Etiam in laoreet odio. \n' +
+    'Nam eget tristique elit, at fermentum tellus. Mauris scelerisque ligula id eleifend feugiat. Donec eleifend vehicula sem nec dapibus. Integer scelerisque neque dui, in lacinia erat molestie eu. Phasellus maximus dui eget lacus porta tempor. Ut ex nibh, dignissim quis purus semper, efficitur facilisis turpis. Praesent blandit elementum ultricies. Aliquam sit amet enim sit amet nulla pulvinar lobortis consectetur non odio. Phasellus at lacus hendrerit, vulputate nisi sit amet, viverra mauris. Etiam eu scelerisque orci. Quisque sagittis, mi vitae pharetra iaculis, orci elit eleifend massa, eu posuere mauris odio id odio. Morbi eu libero luctus, consectetur lorem vel, interdum sapien. Aenean in porta arcu. Maecenas eu maximus massa. \n' +
+    'Praesent velit dolor, dignissim sed quam ac, efficitur porta justo. Pellentesque porta pharetra felis ut hendrerit. Nulla facilisi. Aliquam erat volutpat. Nunc sit amet faucibus quam. Maecenas dapibus luctus dolor at viverra. Duis nec fringilla libero. Duis risus nibh, viverra ac orci nec, iaculis dictum sem. Aliquam at malesuada arcu. Aliquam erat volutpat. Donec varius ipsum purus, ut vehicula purus placerat vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ';
   textoResumoFormatado = '';
-  contResumo = 1;
-  MAXRESUMOFIELD = 923;
+  numParagrafosMostrados: number = 1;
+  MAXRESUMOFIELD = 922;
   universidades = [
     {
       id: 1,
@@ -168,18 +169,16 @@ export class AppComponent {
   ngOnInit() {
     this.notificationNumber();
     this.selectedItemMenu = 5;
+    this.textoResumoArray = this.textoResumo.split('\n');
+    console.log('this.textoResumoArray :>> ', this.textoResumoArray);
     this.formatTextResumo(this.textoResumo);
   }
 
   formatTextResumo(text: string) {
-    this.textoResumoFormatado = text.substring(
-      0,
-      this.MAXRESUMOFIELD * this.contResumo
-    );
-    if (text.length > this.MAXRESUMOFIELD * this.contResumo) {
+    this.textoResumoFormatado = text.substring(0, this.MAXRESUMOFIELD);
+    if (text.length > this.MAXRESUMOFIELD) {
       this.textoResumoFormatado = this.textoResumoFormatado.concat('...');
     }
-    return console.log(text.length);
   }
 
   selectLanguage(itemId: number) {
@@ -199,9 +198,10 @@ export class AppComponent {
   }
 
   verMaisResumo() {
-    if (this.textoResumoFormatado.length < this.textoResumo.length) {
-      this.contResumo += 1;
-      this.formatTextResumo(this.textoResumo);
+    if (this.numParagrafosMostrados !== this.textoResumoArray.length) {
+      this.numParagrafosMostrados = this.textoResumoArray.length;
+    } else {
+      this.numParagrafosMostrados = 1;
     }
   }
 
