8a2d103fddbffba6c15ce6115616662557b08178
lucas.lugao <lucasbrumfurtado@hotmail.com>
2024-05-08 20:56:02 -0300

    fix: melhorando mapeamento de entidades

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -31,6 +31,7 @@ import {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  // ícones
   faGlobe: any = faGlobe;
   faDownload: any = faDownload;
   faStar: any = faStar;
@@ -41,40 +42,8 @@ export class AppComponent {
   faHeartRegular: any = faHeartRegular;
   faCheckDouble: any = faCheckDouble;
   titleSideMenu: string = 'SLACA 2019';
-  optionsSideMenu: SideMenuOptions[] = [
-    { id: 1, label: 'Apresentação' },
-    { id: 2, label: 'Comitês' },
-    { id: 3, label: 'Autores' },
-    { id: 4, label: 'Eixos temáticos' },
-    { id: 5, label: 'Trabalhos' },
-    { id: 6, label: 'Contato' },
-  ];
-  headerData: HeaderData = {
-    firstTitleHeader:
-      'Anais do Simpósio Latino Americano de Ciências de Alimentos',
-    titleHeader:
-      'Anais do 13º Simpósio Latino Americano de Ciência de Alimentos',
-  };
-  issn: string = '2526-4806';
-  languages: Language[] = [
-    { id: 1, label: 'PT, BR' },
-    { id: 2, label: 'EN, US' },
-    { id: 3, label: 'ES, ES' },
-  ];
-  emailUser: string = 'alguem12@galoascience.com';
-  notificationNoRead: number = 0;
-  selectedItemMenu: number = 0;
-  selectedLanguages: number = 1;
-  titlePage: string =
-    'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP ';
-  videoData: VideoData = {
-    titleVideo:
-      'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP ',
-    authorName: 'Beatriz Christiane Melo',
-    authorLocalization: 'FCA / Universidade Estadual de Campinas',
-  };
-  citacaoFavorita: boolean = true;
 
+  // entidades
   detalhesTrabalho: DetalhesTrabalho[] = [
     {
       id: 1,
@@ -92,6 +61,7 @@ export class AppComponent {
       response: 'Alimentos funcionais, alimentação escolar',
     },
   ];
+
   universidades: Universidade[] = [
     {
       id: 1,
@@ -114,6 +84,7 @@ export class AppComponent {
       instituicaoNumber: 4,
     },
   ];
+
   autores: Autor[] = [
     {
       id: 1,
@@ -172,6 +143,7 @@ export class AppComponent {
       ),
     },
   ];
+
   trabalho: Trabalho = {
     id: 1,
     autor: this.autores?.find((autor) => autor.id === 6),
@@ -180,15 +152,46 @@ export class AppComponent {
     eixoTematico: 'Alimentação e saúde (AS) ',
     palavrasChave: 'Alimentos funcionais, alimentação escolar',
   };
-  textoResumoArray: string[] = [];
-  textoResumo: string =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis felis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut. Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum. \n' +
-    'Fusce vitae luctus dui. Donec id euismod mauris, in volutpat urna. Proin dapibus consequat feugiat. Proin dictum justo arcu, quis vestibulum augue lacinia quis. Sed dignissim dui nulla, ut faucibus mauris sodales id. Aliquam erat volutpat. Maecenas dolor enim, tincidunt id elit non, suscipit interdum turpis. Etiam finibus urna libero, eget interdum eros volutpat ullamcorper. Pellentesque et pretium lorem. Aenean interdum quis diam ac porttitor. Cras nec ipsum pulvinar, pharetra libero non, ornare enim. Etiam in laoreet odio. \n' +
-    'Nam eget tristique elit, at fermentum tellus. Mauris scelerisque ligula id eleifend feugiat. Donec eleifend vehicula sem nec dapibus. Integer scelerisque neque dui, in lacinia erat molestie eu. Phasellus maximus dui eget lacus porta tempor. Ut ex nibh, dignissim quis purus semper, efficitur facilisis turpis. Praesent blandit elementum ultricies. Aliquam sit amet enim sit amet nulla pulvinar lobortis consectetur non odio. Phasellus at lacus hendrerit, vulputate nisi sit amet, viverra mauris. Etiam eu scelerisque orci. Quisque sagittis, mi vitae pharetra iaculis, orci elit eleifend massa, eu posuere mauris odio id odio. Morbi eu libero luctus, consectetur lorem vel, interdum sapien. Aenean in porta arcu. Maecenas eu maximus massa. \n' +
-    'Praesent velit dolor, dignissim sed quam ac, efficitur porta justo. Pellentesque porta pharetra felis ut hendrerit. Nulla facilisi. Aliquam erat volutpat. Nunc sit amet faucibus quam. Maecenas dapibus luctus dolor at viverra. Duis nec fringilla libero. Duis risus nibh, viverra ac orci nec, iaculis dictum sem. Aliquam at malesuada arcu. Aliquam erat volutpat. Donec varius ipsum purus, ut vehicula purus placerat vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ';
-  textoResumoFormatado: string = '';
-  numParagrafosMostrados: number = 1;
-  MAXRESUMOFIELD: number = 922;
+
+  optionsSideMenu: SideMenuOptions[] = [
+    { id: 1, label: 'Apresentação' },
+    { id: 2, label: 'Comitês' },
+    { id: 3, label: 'Autores' },
+    { id: 4, label: 'Eixos temáticos' },
+    { id: 5, label: 'Trabalhos' },
+    { id: 6, label: 'Contato' },
+  ];
+
+  headerData: HeaderData = {
+    firstTitleHeader:
+      'Anais do Simpósio Latino Americano de Ciências de Alimentos',
+    titleHeader:
+      'Anais do 13º Simpósio Latino Americano de Ciência de Alimentos',
+  };
+
+  languages: Language[] = [
+    { id: 1, label: 'PT, BR' },
+    { id: 2, label: 'EN, US' },
+    { id: 3, label: 'ES, ES' },
+  ];
+
+  user: User = {
+    id: 1,
+    name: 'Carlos Henrique Santos',
+    topicsAguardandoFeedback: 0,
+  };
+
+  topicEdit: Topico = {
+    id: 0,
+    assunto: '',
+    autorTopico: '',
+    conteudo: '',
+    isLiked: false,
+    curtidas: 0,
+    respostas: 0,
+    aguardandoFeedback: false,
+    dataCriacao: '',
+  };
 
   discussoesTopicos: Topico[] = [
     {
@@ -216,6 +219,31 @@ export class AppComponent {
       dataCriacao: '2024-01-02',
     },
   ];
+
+  // variáveis
+  issn: string = '2526-4806';
+  emailUser: string = 'alguem12@galoascience.com';
+  notificationNoRead: number = 0;
+  selectedItemMenu: number = 0;
+  selectedLanguages: number = 1;
+  titlePage: string =
+    'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP ';
+  videoData: VideoData = {
+    titleVideo:
+      'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP ',
+    authorName: 'Beatriz Christiane Melo',
+    authorLocalization: 'FCA / Universidade Estadual de Campinas',
+  };
+  citacaoFavorita: boolean = true;
+  textoResumoArray: string[] = [];
+  textoResumo: string =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis felis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut. Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum. \n' +
+    'Fusce vitae luctus dui. Donec id euismod mauris, in volutpat urna. Proin dapibus consequat feugiat. Proin dictum justo arcu, quis vestibulum augue lacinia quis. Sed dignissim dui nulla, ut faucibus mauris sodales id. Aliquam erat volutpat. Maecenas dolor enim, tincidunt id elit non, suscipit interdum turpis. Etiam finibus urna libero, eget interdum eros volutpat ullamcorper. Pellentesque et pretium lorem. Aenean interdum quis diam ac porttitor. Cras nec ipsum pulvinar, pharetra libero non, ornare enim. Etiam in laoreet odio. \n' +
+    'Nam eget tristique elit, at fermentum tellus. Mauris scelerisque ligula id eleifend feugiat. Donec eleifend vehicula sem nec dapibus. Integer scelerisque neque dui, in lacinia erat molestie eu. Phasellus maximus dui eget lacus porta tempor. Ut ex nibh, dignissim quis purus semper, efficitur facilisis turpis. Praesent blandit elementum ultricies. Aliquam sit amet enim sit amet nulla pulvinar lobortis consectetur non odio. Phasellus at lacus hendrerit, vulputate nisi sit amet, viverra mauris. Etiam eu scelerisque orci. Quisque sagittis, mi vitae pharetra iaculis, orci elit eleifend massa, eu posuere mauris odio id odio. Morbi eu libero luctus, consectetur lorem vel, interdum sapien. Aenean in porta arcu. Maecenas eu maximus massa. \n' +
+    'Praesent velit dolor, dignissim sed quam ac, efficitur porta justo. Pellentesque porta pharetra felis ut hendrerit. Nulla facilisi. Aliquam erat volutpat. Nunc sit amet faucibus quam. Maecenas dapibus luctus dolor at viverra. Duis nec fringilla libero. Duis risus nibh, viverra ac orci nec, iaculis dictum sem. Aliquam at malesuada arcu. Aliquam erat volutpat. Donec varius ipsum purus, ut vehicula purus placerat vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ';
+  textoResumoFormatado: string = '';
+  numParagrafosMostrados: number = 1;
+  MAXRESUMOFIELD: number = 922;
   textFooter: string[] = [
     'Preservar a memória do evento e ampliar o acesso ao conhecimento ' +
       'científico gerado em eventos é a razão de ser da plataforma Galoá' +
@@ -226,22 +254,6 @@ export class AppComponent {
   ];
   criandoTopico: boolean = false;
   editandoTopico: boolean = false;
-  topicEdit: Topico = {
-    id: 0,
-    assunto: '',
-    autorTopico: '',
-    conteudo: '',
-    isLiked: false,
-    curtidas: 0,
-    respostas: 0,
-    aguardandoFeedback: false,
-    dataCriacao: '',
-  };
-  user: User = {
-    id: 1,
-    name: 'Carlos Henrique Santos',
-    topicsAguardandoFeedback: 0,
-  };
   assuntoTopicoField: string = '';
   conteudoTopicoField: string = '';
 
