24030f54cd04137a3052a0a45300b3fc178b732b
Lucas Viegas <lucas.viegas3005@gmail.com>
2024-05-08 20:07:57 -0300

    commit inicial - add xpath

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -10,14 +10,33 @@ class Main {
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
-  public static function run(): void {
-    $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+   public static function run(): void {
+  //   $dom = new \DOMDocument('1.0', 'utf-8');
+  //   $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
+  //   $data = (new Scrapper())->scrap($dom);
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
+  //   // Write your logic to save the output file bellow.
+  $dom = new \DOMDocument('1.0', 'utf-8');
+  $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+
+  $xpath = new \DomXPath($dom);
+  $nodeList = $xpath->query("//a']");
+  $node = $nodeList->item(0);
+  $conteudoDascaixas = $node->nodeValue;
+
+
+  foreach ($nodelist as $conteudoDascaixas){
+    $href = $xpath->query('//a', $n)->item(0)->getAttribute('href');
+    $a_text = $xpath->query("//h4[@class='paper-title']", $n)->item(0)->nodeValue;
+    $div_text = $xpath->query('h4', $n)->item(0)->nodeValue;}
+
+  print("\n\n\n\n\n\n\n\n");
+  print_r($conteudoDascaixas);
+  print("\n\n\n\n\n\n\n\n");
+    
+  //   print_r($data);
+  
   }
 
 }
