9ea8b246555f7cb405173b088beadbcfa6f4d3d0
lucas.lugao <lucasbrumfurtado@hotmail.com>
2024-05-20 21:45:52 -0300

    testando metodo de teste

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -43,7 +43,7 @@ export class AppComponent {
   faHeartRegular: any = faHeartRegular;
   faCheckDouble: any = faCheckDouble;
 
-  // entidades
+  // entidades.
   detalhesTrabalho: DetalhesTrabalho[] = [
     {
       id: 1,
